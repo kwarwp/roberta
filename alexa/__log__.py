7c4b89a9706e07831abf792e57172718f422c4d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:50:14 -0300

    Created alexa/__log__.py @2020-09-14 23:50

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 14 2020 20:50:13.468 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  )
+  ^
+IndentationError: unexpected indent
+'''},
