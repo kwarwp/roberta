4f2d3a77811e974cf40b9500bf1f60be8cbbca2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:37:52 -0300

    Saved grace/__log__.py @2020-09-14 23:37

--- grace/__log__.py
+++ grace/__log__.py
@@ -34,3 +34,10 @@ SyntaxError: invalid syntax
      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 14 2020 20:37:49.350 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  .. codeauthor:: mariaclara <mari4cla@gmail.com>
+     ^
+SyntaxError: invalid syntax
+'''},
