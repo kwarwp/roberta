55f8ee86af58e27314053859abfcb369c3d878ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 17:05:59 -0300

    Created tutorial/cenas.py @2020-09-14 20:05

---
+++ tutorial/cenas.py
@@ -0,0 +1,36 @@
+# janese.cenas.imix.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""     Vitollino na UNESP 2020
+
+.. codeauthor:: Equipe Supygirls <supygirls@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.08
+       - Cenas;
+       - Texto de abertura;
+       - Elemento;
+       - Texto ao clicar no elemento;
+       - Importação de classes de MESMO MÓDULO
+
+"""
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+#from samantha.main import turmaDireita
+#from kristen.main import inicialesquerda
+#from danae.main import Lago
+
+FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
+LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
+
+STYLE["width"] = 900
+STYLE["heigth"] = 900
+
+#cena_fundo = Cena(FUNDO)
+#cena_fundo.vai()
+
+#def cria_fundo():
+    #cena_fundo = Cena(FUNDO)
+    #cena_fundo.vai()
+    
+    
+#cria_fundo()
