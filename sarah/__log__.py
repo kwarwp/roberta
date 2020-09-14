3bbdb667160abe84f8f8107ddf67c08683abdef2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:51:58 -0300

    Created sarah/__log__.py @2020-09-14 23:51

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 14 2020 20:51:25.319 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  STYLE["height'] = "900px"
+                      ^
+SyntaxError: invalid syntax
+'''},
