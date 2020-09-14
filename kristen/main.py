eb4334367c24a7b809a9023fdf0dadcdfe4a4723
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:52:56 -0300

    Saved kristen/main.py @2020-09-14 23:52

--- kristen/main.py
+++ kristen/main.py
@@ -12,8 +12,8 @@ Changelog
 
 from _spy.vitollino.main import Cena, STYLE
 
-STYLE ["with"] = 3000
-STYLE ["heigh"] = 3000
+STYLE ["width"] = 3000
+STYLE ["heigth"] = 3000
 
 FLORESTA = "https://image.freepik.com/vetores-gratis/folhagem-de-floresta-tropical-paisagem_23-2148271243.jpg"
 
