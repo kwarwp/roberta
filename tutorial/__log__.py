ec9d643e0869b4e2c476e7be504db758ba77a4f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 18:35:55 -0300

    Saved tutorial/__log__.py @2020-09-14 21:35

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -87,3 +87,19 @@ NameError: name 'FUNDO' is not defined
     cena_fundo = Cena(FUNDO)
 NameError: name 'FUNDO' is not defined
 '''},
+{'date': 'Mon Sep 14 2020 18:35:54.673 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    elemento = Elemento(ELEMENTO, w=100, h=100,x=150,y=200,vai=cena_esquerda)
+NameError: name 'Elemento' is not defined
+'''},
