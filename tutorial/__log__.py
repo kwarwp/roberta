c9051592942a6a2d68e658cabef4ee1e5f9e2bbb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 17:35:31 -0300

    Saved tutorial/__log__.py @2020-09-14 17:35

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -31,3 +31,21 @@
 'error': ''''''},
 {'date': 'Mon Sep 14 2020 17:30:48.58 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
 'error': ''''''},
+{'date': 'Mon Sep 14 2020 17:35:29.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 178
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 136
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 311
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 283
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 300
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    cria_fundo()
+  module <module> line 26
+    cena_fundo = Cena(FUNDO)
+NameError: name 'FUNDO' is not defined
+'''},
