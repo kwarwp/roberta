9239f1b33780ce3736ae57fa32fa089954266639
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:13:52 -0300

    Created adda/main.py @2020-09-14 23:13

---
+++ adda/main.py
@@ -0,0 +1,14 @@
+# roberta.adda.main.py
+"""xxxx
+..codeauthor: Isabel Hortencia Garnica <hortencia.garnica@gmail.com>
+
+Changelog
+---------
+..versionadded::
+    - xxx
+    
+"""
+numero_um = 1
+numero_dois = 2.5
+soma = numero_um + numero_dois
+print(soma)
