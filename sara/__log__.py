2aa5d0cc1f9c7429fdeb07e26455938a448d444a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:49:17 -0300

    Saved sara/__log__.py @2020-09-14 23:49

--- sara/__log__.py
+++ sara/__log__.py
@@ -30,3 +30,19 @@ Traceback (most recent call last):
   module <module> line 13
     from _spy.vitollino.main  import CENA, STYLE
 '''},
+{'date': 'Mon Sep 14 2020 20:49:15.391 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    floresta_amz= CENA(floresta)
+NameError: name 'CENA' is not defined
+'''},
