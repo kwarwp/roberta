b1dd535bdbe601e45a2e0efc923c2b837d98f9ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:57:41 -0300

    Created adda/__log__.py @2020-09-14 23:57

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 14 2020 20:57:35.114 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  floresta_verde  Cena(FLORESTA)
+                   ^
+SyntaxError: invalid syntax
+'''},
