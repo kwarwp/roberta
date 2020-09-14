3721c5db751fa7230160294b3501ab07b60c67ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:48:07 -0300

    Saved sara/main.py @2020-09-14 23:48

--- sara/main.py
+++ sara/main.py
@@ -10,4 +10,13 @@ Changelog
 
 """
 
-2**38
+from _spy.vitollino.main  import CENA, STYLE
+
+STYLE["width"] = 900 # width = 300 (default)
+STYLE["heigth"] = "900px" # min-height = "300px"
+
+floresta= "http://1.bp.blogspot.com/-lGK-NZbMSBE/UUtrchW3_-I/AAAAAAAAIs4/hsWa_5_A-1g/s1600/floresta-amazonica.jpg"
+
+floresta_amz= CENA(floresta)
+
+floresta_amz.vai()
