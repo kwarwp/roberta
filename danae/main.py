c1436b17f0f22c057af7e774524bd9f1212330ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 19:50:24 -0300

    Created danae/main.py @2020-09-14 22:50

---
+++ danae/main.py
@@ -0,0 +1,20 @@
+# roberta.danae.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto inicial do tutorial Python.
+
+.. codeauthor:: Carlo <carlo@nce.ufrj.br>
+
+Changelog
+---------
+.. versionadded::    20.09
+        Descreva o que você adicionou no código.
+
+"""
+
+
+class Tutorial:
+    pass
+    
+    
+if __name__ == "__main__":
+    Tutorial()
