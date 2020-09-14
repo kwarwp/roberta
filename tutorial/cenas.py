aca50e0f49036c948a1391023eaa2d05300c4042
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 17:37:37 -0300

    Saved tutorial/cenas.py @2020-09-14 20:37

--- tutorial/cenas.py
+++ tutorial/cenas.py
@@ -11,7 +11,6 @@ ARCO = "https://i.ytimg.com/vi/ynxZPR27gi4/maxresdefault.jpg"
 LAPIS = "https://i.ytimg.com/vi/5igYewGEoFo/maxresdefault.jpg"
 
 
-
 STYLE["width"] = 900
 STYLE["heigth"] = "900px"
 
@@ -22,7 +21,7 @@ STYLE["heigth"] = "900px"
 #cena_fundo = Cena(FUNDO)
 #cena_fundo.vai()
 
-def cria_fundo():
+def cria_fundo(*_):
     cena_fundo = Cena(FUNDO)
     cena_fundo.vai()
     
