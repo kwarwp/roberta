4065a2b4220870ceda2eb99d7f128b87cc522781
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 17:10:29 -0300

    Saved tutorial/cenas.py @2020-09-14 20:10

--- tutorial/cenas.py
+++ tutorial/cenas.py
@@ -19,13 +19,19 @@ from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 #from kristen.main import inicialesquerda
 #from danae.main import Lago
 
-FUNDO = "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
-LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
+MUNDO = "http://www.pngall.com/wp-content/uploads/1/World-Map.png"
+ARCO = "https://i.ytimg.com/vi/ynxZPR27gi4/maxresdefault.jpg"
+CIRCULO = "https://i.ytimg.com/vi/n6_0ipbjBYI/maxresdefault.jpg"
+LAPIS = "https://i.ytimg.com/vi/5igYewGEoFo/maxresdefault.jpg"
+
 
 STYLE["width"] = 900
 STYLE["heigth"] = 900
 
-cena_fundo = Cena(FUNDO)
+cena_meio = Cena(CIRCULO)
+cena_direita = Cena(ARCO)
+cena_fundo = Cena(MUNDO, meio=cena_meio, direita = cena_direita)
+
 cena_fundo.vai()
 
 #def cria_fundo():
