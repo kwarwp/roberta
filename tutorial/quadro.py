4ed089d9d5c5d133522ed67ec4a8d1ae4e40ed54
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:44:05 -0300

    Saved tutorial/quadro.py @2020-09-14 23:44

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -1,4 +1,22 @@
 # roberta.tutorial.quadro.py
 # SPDX-License-Identifier: GPL-3.0-or-later
+"""     xxxxx
 
+.. codeauthor:: Emanuelle Simas <ellesimas@gmail.com>
 
+Changelog
+---------
+.. versionadded::    
+       - xxx
+
+"""
+from _spy.vitollino.main import Cena 
+
+STYLE["width"] = 900 
+STYLE["heigth"] = "900px" 
+
+FLORESTA = "https://upload.wikimedia.org/wikipedia/commons/4/4c/Po%C3%A7a_das_Asas%2C_floresta%2C_concelho_da_Horta%2C_ilha_do_Faial%2C_A%C3%A7ores%2C_Portugal.JPG"
+
+floresta_verde = Cena(FLORESTA)
+
+floresta_verde.vai()
