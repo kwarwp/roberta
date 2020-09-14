4519e3fe64b8afba81504041bc99796d606a8415
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:49:51 -0300

    Saved kristen/main.py @2020-09-14 23:49

--- kristen/main.py
+++ kristen/main.py
@@ -15,7 +15,7 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE ["with"] = 900
 STYLE ["heigh"] = "900px"
 
-FLORESTA = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fpng-mc293m%2F&psig=AOvVaw3QnxGW_oIl1YFSLdgHdbKu&ust=1600213718070000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiE5-Tq6esCFQAAAAAdAAAAABAD.png"
+FLORESTA = "https://image.freepik.com/vetores-gratis/folhagem-de-floresta-tropical-paisagem_23-2148271243.jpg"
 
 floresta_verde = Cena(FLORESTA)
 
