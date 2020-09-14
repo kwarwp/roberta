9b757c515cd8069af102bad9824e65a1115c4f04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:49:06 -0300

    Saved kristen/main.py @2020-09-14 23:49

--- kristen/main.py
+++ kristen/main.py
@@ -9,3 +9,14 @@ Changelog
        - xxx
 
 """
+
+from _spy.vitollino.main import Cena, STYLE
+
+STYLE ["with"] = 900
+STYLE ["heigh"] = "900px"
+
+FLORESTA = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fpng-mc293m%2F&psig=AOvVaw3QnxGW_oIl1YFSLdgHdbKu&ust=1600213718070000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiE5-Tq6esCFQAAAAAdAAAAABAD.png"
+
+floresta_verde = Cena(FLORESTA)
+
+floresta_verde.vai()
