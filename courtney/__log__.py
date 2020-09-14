9466ff28d6676b944ed65019682ed5c991c01044
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:27:40 -0300

    Created courtney/__log__.py @2020-09-14 23:27

---
+++ courtney/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Mon Sep 14 2020 20:27:31.394 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    print(operacao)
+NameError: name 'operacao' is not defined
+'''},
