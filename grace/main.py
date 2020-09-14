17a5a6a22d340286f64499365de097d00ec9cd5c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:10:16 -0300

    Saved grace/main.py @2020-09-14 23:10

--- grace/main.py
+++ grace/main.py
@@ -8,3 +8,13 @@ Changelog
        - xxx
 
 """
+Define uma Sala ou uma Cena vazia.
+    .. doctest::
+        >>> cena = Cena(SalaCenaNula())  # A próxima cena
+        >>> uma_cena = Cena(SalaCenaNula(), cena)  # Cena nula à esquerda, proxima no meio
+        >>> uma_cena.vai_esquerda()  # tenta navegar para a cena à esquerda
+        >>> # não vai, pois a cena é nula e não deixa que se navegue para ela
+        >>> print(INVENTARIO.cena == cena)
+        True
+  
+    """
