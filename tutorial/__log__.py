bb6e3a4072cdba27f8cac7f617ca0961387d69c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:30:52 -0300

    Saved tutorial/__log__.py @2020-09-14 23:30

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -135,3 +135,19 @@ TypeError: unsupported operand type(s) for +: 'int' and 'str'
     operacao = numero_um + letras
 TypeError: unsupported operand type(s) for +: 'int' and 'str'
 '''},
+{'date': 'Mon Sep 14 2020 20:30:52.193 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    print(operacao)
+NameError: name 'operacao' is not defined
+'''},
