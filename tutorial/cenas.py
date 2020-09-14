9d60318ac9d4bcb9c23df1773ed1e5bbb83c0bbb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 17:54:19 -0300

    Saved tutorial/cenas.py @2020-09-14 20:54

--- tutorial/cenas.py
+++ tutorial/cenas.py
@@ -14,18 +14,16 @@ LAPIS = "https://i.ytimg.com/vi/5igYewGEoFo/maxresdefault.jpg"
 STYLE["width"] = 900
 STYLE["heigth"] = "900px"
 
-#cena_direita = Cena(ARCO)
-#cena_esquerda =Cena(LAPIS)
-#cena_fundo = Cena(MUNDO, esquerda=cena_esquerda, direita=cena_direita)
+cena_direita = Cena(ARCO)
+cena_esquerda =Cena(LAPIS)
+cena_fundo = Cena(MUNDO, esquerda=cena_esquerda, direita=cena_direita)
+#cena_fundo = Cena(MUNDO)
+cena_fundo.vai()
 
-#cena_fundo = Cena(FUNDO)
-#cena_fundo.vai()
-
-def cria_fundo(*_):
-    cena_direita = Cena(ARCO)
-    cena_esquerda =Cena(LAPIS)
-    cena_fundo = Cena(MUNDO, direita=cena_direita, esquerda=cena_esquerda)
-    cena_fundo.vai()
-    
-    
-cria_fundo()
+#def cria_fundo(*_):
+    #cena_direita = Cena(ARCO)
+    #cena_esquerda =Cena(LAPIS)
+    #cena_fundo = Cena(MUNDO, direita=cena_direita, esquerda=cena_esquerda)
+    #cena_fundo.vai()
+        
+#cria_fundo()
