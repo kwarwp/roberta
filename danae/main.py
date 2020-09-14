24eff804310a5b5dba8f2f9e5e66c4b6078cc8f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 20:58:08 -0300

    Saved danae/main.py @2020-09-14 23:58

--- danae/main.py
+++ danae/main.py
@@ -17,11 +17,15 @@ from _spy.vitollino.main import STYLE, Cena
 STYLE["width"] = 900 # width = 300 (default)
 STYLE["height"] = "900px" # min-height = "300px"
 CENA = "https://i.imgur.com/gSzqe5C.jpg"
+ESQU = "https://i.imgur.com/aisVckn.jpg"
+DIRE = "https://i.imgur.com/0yzM8ue.jpg"
 
 
 class Tutorial:
     def __init__(self):
-        self.cena = Cena(CENA)
+        esqu = Cena(ESQU)
+        dire = Cena(DIRE)
+        self.cena = Cena(CENA, direita=dire, esquerda=esqu)
         
     def vai(self):
         self.cena.vai()
