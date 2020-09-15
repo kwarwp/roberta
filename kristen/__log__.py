ca050c5b3b65b37d007290074a159387493fb3ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:15:08 -0300

    Saved kristen/__log__.py @2020-09-15 00:15

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -6,3 +6,10 @@
                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 14 2020 21:15:08.284 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 25
+  borboleta = Cena(BORBOLETA, esquerda = florest_verde
+                                                         ^
+SyntaxError: invalid syntax
+'''},
