1c000d55c67d3b4db95315f3a08e23bba448b967
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:02:44 -0300

    Saved alexa/__log__.py @2020-09-15 00:02

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Sep 14 2020 21:02:44.517 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 28
+  nome_da_cena_meio = Cena(IMAGEM_MEIO, esquerda = )
+                                                    ^
+SyntaxError: invalid syntax
+'''},
