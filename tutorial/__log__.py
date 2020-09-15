af05019c47259efabf07dea5ca596dda804f6b71
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 22:40:13 -0300

    Saved tutorial/__log__.py @2020-09-15 01:40

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -267,3 +267,19 @@ UnboundLocalError: local variable 'mundo' referenced before assignment
     mundo = Cena(MUNDO, esquerda = floresta_verde)
 UnboundLocalError: local variable 'floresta_verde' referenced before assignment
 '''},
+{'date': 'Mon Sep 14 2020 22:40:11.821 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    ambiente()
+TypeError: ambiente missing 1 positional argument: 'self'
+'''},
