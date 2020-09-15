eca294cfea5d23178a6998ae1faf15902a299d30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 23:10:39 -0300

    Saved grace/__log__.py @2020-09-15 02:10

--- grace/__log__.py
+++ grace/__log__.py
@@ -215,3 +215,21 @@ NameError: name 'arco' is not defined
     	floresta_verde - cena(Floresta, direita - mundo, esquerda - arco, meio - lapis)
 NameError: name 'floresta_verde' is not defined
 '''},
+{'date': 'Mon Sep 14 2020 23:10:35.582 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    ambiente()
+  module <module> line 31
+    	floresta_verde = cena(Floresta, direita - mundo, esquerda - arco, meio - lapis)
+NameError: name 'cena' is not defined
+'''},
