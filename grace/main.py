afa170a30bccc985d43389eb2e5127f1324b01a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 23:25:17 -0300

    Saved grace/main.py @2020-09-15 02:25

--- grace/main.py
+++ grace/main.py
@@ -18,7 +18,7 @@ STYLE["width"] = 300 # width = 300 (default)
 STYLE["heigth"] = "900px" # min-height = "300px"
 
 FLORESTA= 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia24412/floresta-cpt.jpg'
-MUNDO= 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png?x11594'
+MUNDO= 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png'
 ARCO= 'https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg'
 LAPIS= "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg"
 
