51c21544484f900d7260466d56caf5dde6901047
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 23:01:46 -0300

    Saved grace/main.py @2020-09-15 02:01

--- grace/main.py
+++ grace/main.py
@@ -1,12 +1,13 @@
-# roberta.grace.main.py"""     xxxxx
+# roberta.grace.main.py"""   
+"""     xxxxx
 
-".. codeauthor:: mariaclara < mari4cla@gmail.com >
+.. codeauthor:: mariaclara < mari4cla@gmail.com >
 
-"Changelog
-"---------
-".. versionadded::    
-"       - xxx
-
+Changelog
+---------
+.. versionadded::    
+       - xxx
+"""
 
 
 from _spy.vitollino.main import Cena,STYLE
@@ -16,10 +17,10 @@ from _spy.vitollino.main import Cena,STYLE
 STYLE["width"] = 900 # width = 300 (default)
 STYLE["heigth"] = "900px" # min-height = "300px"
 
-FLORESTA: https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia24412/floresta-cpt.jpg
-MUNDO: https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png?x11594
-ARCO: https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg
-LAPIS: https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg
+FLORESTA: 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia24412/floresta-cpt.jpg'
+MUNDO: 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png?x11594'
+ARCO: "https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg'
+LAPIS: "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg"
 
 arco - cena(ARCO)
 lapis - cena(LAPIS)
