f45509375ffe95c5c647169496e249caf652d047
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:09:06 -0300

    Created callie/__log__.py @2020-09-15 00:09

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 14 2020 21:08:28.914 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  MAR = https://www.google.com/url?sa=i&url=https%3A%2F%2Fescolaeducacao.com.br%2Fqual-a-diferenca-entre-mar-e-oceano%2F&psig=AOvVaw1-yzoNo08Ga6PCJK-R23xc&ust=1600213379864000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiE3NTp6esCFQAAAAAdAAAAABAL
+              ^
+SyntaxError: invalid syntax
+'''},
