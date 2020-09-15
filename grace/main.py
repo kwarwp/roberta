ab06d7efe86e39dff8f8fdd78816de9be6dcfdde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 00:08:36 -0300

    Saved grace/main.py @2020-09-15 03:08

--- grace/main.py
+++ grace/main.py
@@ -22,15 +22,15 @@ MUNDO= 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa
 ARCO= 'https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg'
 LAPIS= "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg"
 
-floresta= Cena(FLORESTA) 
-mundo= Cena(MUNDO)
-arco = Cena(ARCO)
-lapis = Cena(LAPIS)
+#floresta= Cena(FLORESTA) 
+#mundo= Cena(MUNDO)
+#arco = Cena(ARCO)
+#lapis = Cena(LAPIS)
 
 #floresta_verde - cena(floresta, direita - mundo, esquerda - arco, meio - lapis) 
 
 def ambiente():
-      floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco, meio = lapis)
+      floresta_verde = Cena(FLORESTA), direita = Cena(MUNDO), esquerda = Cena(ARCO), meio = Cena(LAPIS))
       mundo = Cena(MUNDO, esquerda = floresta_verde) 
       floresta_verde.vai()
 
