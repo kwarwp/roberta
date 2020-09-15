14332e654dc28cad0876c9bbf5cff9df93939095
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 14:31:04 -0300

    Saved callie/main.py @2020-09-15 17:31

--- callie/main.py
+++ callie/main.py
@@ -9,3 +9,11 @@ Changelog
        - xxx
 
 """
+STYLE["width"] = 900 
+STYLE["heigth"] = "900px" 
+
+MAR = https://www.google.com/url?sa=i&url=https%3A%2F%2Fescolaeducacao.com.br%2Fqual-a-diferenca-entre-mar-e-oceano%2F&psig=AOvVaw1-yzoNo08Ga6PCJK-R23xc&ust=1600213379864000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiE3NTp6esCFQAAAAAdAAAAABAL
+
+mar_azul = cena(MAR)
+
+mar_azul.vai()
