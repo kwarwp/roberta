03a3aacd928186abcc0ac26c28e9974d36f0c641
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 22:09:10 -0300

    Saved grace/__log__.py @2020-09-15 01:09

--- grace/__log__.py
+++ grace/__log__.py
@@ -132,3 +132,10 @@ SyntaxError: invalid syntax
                                                     ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Sep 14 2020 22:09:07.568 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  ".. codeauthor:: mariaclara < mari4cla@gmail.com >
+                                                    ^
+SyntaxError: EOL while scanning string literal
+'''},
