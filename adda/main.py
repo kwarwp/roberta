b31f207ded4b4bcc2b8ae624685f575e8346aa0d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:23:27 -0300

    Saved adda/main.py @2020-09-15 00:23

--- adda/main.py
+++ adda/main.py
@@ -16,8 +16,9 @@ MUNDO = "https://i.imgur.com/KYGCskB.gifv"
 ARCO = "https://i.imgur.com/PdAD7GC.jpg"
 LAPIS = "https://i.imgur.com/U7w5CPr.png"
 
+floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco)
 mundo = Cena(MUNDO, esquerda = floresta_verde)
 arco = Cena(ARCO, direita= floresta_verde)
 
-floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco, meio= Cena(LAPIS))
+
 floresta_verde.vai()
