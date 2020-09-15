ee9aa736763324aeaf4a8306044cc62133412ce4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 23:45:05 -0300

    Saved tutorial/__log__.py @2020-09-15 02:45

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -433,3 +433,19 @@ NameError: name 'floresta_verde' is not defined
     mundo = Cena(MUNDO, esquerda = floresta_verde)
 NameError: name 'floresta_verde' is not defined
 '''},
+{'date': 'Mon Sep 14 2020 23:45:04.641 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    ambiente()
+NameError: name 'ambiente' is not defined
+'''},
