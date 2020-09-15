b385006d7d23dddc75862ae30bb620d187e56074
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:00:57 -0300

    Saved sarah/__log__.py @2020-09-15 00:00

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -37,3 +37,19 @@ Traceback (most recent call last):
   module <module> line 12
     from _spy.vitollino.main import Cena, Style
 '''},
+{'date': 'Mon Sep 14 2020 21:00:24.861 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco, meio = lapis)
+NameError: name 'lapis' is not defined
+'''},
