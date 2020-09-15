2b880c67dd64e7c2488084d7d82cdbba4b5fce9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:21:36 -0300

    Saved adda/main.py @2020-09-15 00:21

--- adda/main.py
+++ adda/main.py
@@ -12,9 +12,9 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE["width"] = 900
 STYLE["height"] = "900px"
 FLORESTA = "https://imgur.com/a/vQzh2NP"
-MUNDO = ""
-ARCO = ""
-LAPIS = ""
+MUNDO = "https://i.imgur.com/KYGCskB.gifv"
+ARCO = "https://i.imgur.com/PdAD7GC.jpg"
+LAPIS = "https://i.imgur.com/U7w5CPr.png"
 
 mundo = Cena(MUNDO, esquerda = floresta_verde)
 arco = Cena(ARCO, direita= floresta_verde)
