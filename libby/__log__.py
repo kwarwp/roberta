1c003fa0421d22fbfa918b9c382d9a1e3e09b3f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:14:05 -0300

    Saved libby/__log__.py @2020-09-15 00:14

--- libby/__log__.py
+++ libby/__log__.py
@@ -22,3 +22,19 @@ NameError: name 'imagem_esquerda' is not defined
                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 14 2020 21:14:03.857 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    floresta_verde= Cena(FLORESTA, direita= lemori, esquerda= cachorro, meio = gato)
+NameError: name 'gato' is not defined
+'''},
