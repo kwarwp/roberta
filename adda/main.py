9bc4b067f1dd81562f3b686c47af0e057ca4f604
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:45:37 -0300

    Saved adda/main.py @2020-09-15 00:45

--- adda/main.py
+++ adda/main.py
@@ -12,12 +12,15 @@ from _spy.vitollino.main import Cena, STYLE
 STYLE["width"] = 1100
 STYLE["height"] = "600px"
 FLORESTA = "https://i.imgur.com/4lB1eyQ.jpg"
-MUNDO = "https://i.imgur.com/KYGCskB.gifv"
+MUNDO = "https://i.imgur.com/LpWmClc.png"
 ARCO = "https://i.imgur.com/PdAD7GC.jpg"
 LAPIS = "https://i.imgur.com/U7w5CPr.png"
 
 def flora():
     cenaFloresta = Cena(img = FLORESTA)
     
+    cenaMundo = Cena(img = MUNDO)
+    cenaFloresta.direita = cenaMundo
+    cenaMundo.vai()
     cenaFloresta.vai()
 flora()
