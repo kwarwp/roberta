1946c3f463ca0081689e290518d03e77a1649b08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 21:59:29 -0300

    Saved tutorial/quadro.py @2020-09-15 00:59

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -10,13 +10,30 @@ Changelog
        - xxx
 
 """
-from _spy.vitollino.main import Cena 
+from _spy.vitollino.main import Cena, STYLE 
 
 STYLE["width"] = 900 
 STYLE["heigth"] = "900px" 
 
 FLORESTA = "https://upload.wikimedia.org/wikipedia/commons/4/4c/Po%C3%A7a_das_Asas%2C_floresta%2C_concelho_da_Horta%2C_ilha_do_Faial%2C_A%C3%A7ores%2C_Portugal.JPG"
+MUNDO = "http://www.pngall.com/wp-content/uploads/1/World-Map.png"
+ARCO = "https://i.ytimg.com/vi/ynxZPR27gi4/maxresdefault.jpg"
+LAPIS = "https://i.ytimg.com/vi/5igYewGEoFo/maxresdefault.jpg"
 
-floresta_verde = Cena(FLORESTA)
+#arco = Cena(ARCO)
+#lapis = Cena(LAPIS)
 
-floresta_verde.vai()
+
+#floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco, meio = lapis)
+
+def ambiente():
+    floresta_verde = Cena(FLORESTA, direita = mundo, esquerda = arco, meio = lapis)
+    mundo = Cena(MUNDO, esquerda = floresta_verde)
+    floresta_verde.vai()
+    
+ambiente()
+
+
+
+
+
