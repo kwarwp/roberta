f7fd15dd02a477c8e53f2373fd9c5527db57b457
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 01:11:18 -0300

    Saved meredith/__log__.py @2020-09-18 04:11

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -51,3 +51,19 @@ NameError: name 'Caminho_esquerdo' is not defined
     Floresta = Cena('https://i.ytimg.com/vi/dL9_IQv3eGE/maxresdefault.jpg',
 UnboundLocalError: local variable 'Caminho_direito' referenced before assignment
 '''},
+{'date': 'Fri Sep 18 2020 01:11:15.682 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    Caminho_direito  = Cena('https://i.pinimg.com/originals/f1/1a/18/f11a183f87fab48d6ac3024979fa2d9d.jpg', esquerdo=Floresta)
+NameError: name 'Floresta' is not defined
+'''},
