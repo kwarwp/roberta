5a02435342689f0f072280438685df606180d471
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 21:03:27 -0300

    Created heather/__log__.py @2020-09-18 00:03

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Sep 17 2020 21:03:23.845 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  print(val[0].__add__(val[]) 
+                                                                           ^
+SyntaxError: invalid syntax
+'''},
