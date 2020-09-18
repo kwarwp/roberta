44bfb52b16f17807fa348d8b9c132089abf7f5d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 21:16:16 -0300

    Saved heather/__log__.py @2020-09-18 00:16

--- heather/__log__.py
+++ heather/__log__.py
@@ -68,3 +68,20 @@ NameError: name 'opera_soma_pesos' is not defined
           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Sep 17 2020 21:16:15.40 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''(['antoni', 'lucia'], 70)
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    print(Numeros.val)
+AttributeError: 'Numeros' object has no attribute 'val'
+'''},
