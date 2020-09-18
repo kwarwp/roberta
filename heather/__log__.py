43ad6596538370601c2415c5a49ffca7c4d47049
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 21:09:35 -0300

    Saved heather/__log__.py @2020-09-18 00:09

--- heather/__log__.py
+++ heather/__log__.py
@@ -45,3 +45,19 @@ NameError: name 'numeros' is not defined
     print("O tipo do 'operacao_um' é:", type(operacao_um)) #NoneType
 NameError: name 'operacao_um' is not defined
 '''},
+{'date': 'Thu Sep 17 2020 21:09:33.366 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    opera_soma_pesos(antoni=40,lucia=30)
+NameError: name 'opera_soma_pesos' is not defined
+'''},
