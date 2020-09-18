c8737262d567fed8ef8bb4cc4c22b7fda681e6e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 01:54:43 -0300

    Saved meredith/__log__.py @2020-09-18 04:54

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -121,3 +121,21 @@ UnboundLocalError: local variable 'Caminho_direito' referenced before assignment
     caminho_direito=Cena(img_direita)
 NameError: name 'img_direita' is not defined
 '''},
+{'date': 'Fri Sep 18 2020 01:54:40.748 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    ambiente()
+  module <module> line 50
+    caminho_direito=Cena(img_direita)
+NameError: name 'img_direita' is not defined
+'''},
