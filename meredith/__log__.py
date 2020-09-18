7c674fb35752bdc8d8180de9f87b36f3e8c6b4ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 01:08:42 -0300

    Saved meredith/__log__.py @2020-09-18 04:08

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -15,3 +15,21 @@
     divide     = trib_a / atrib_b
 NameError: name 'trib_a' is not defined
 '''},
+{'date': 'Fri Sep 18 2020 01:08:40.81 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    ambiente()
+  module <module> line 45
+    Floresta = Cena('https://i.ytimg.com/vi/dL9_IQv3eGE/maxresdefault.jpg',
+NameError: name 'Caminho_esquerdo' is not defined
+'''},
