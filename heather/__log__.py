0aeb4b072f153223ada113d7872b4c65b1e9beab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 21:09:10 -0300

    Saved heather/__log__.py @2020-09-18 00:09

--- heather/__log__.py
+++ heather/__log__.py
@@ -6,3 +6,10 @@
                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Sep 17 2020 21:09:09.221 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  print(val[0].__add__(val[1])
+                                                                           ^
+SyntaxError: invalid syntax
+'''},
