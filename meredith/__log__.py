04c897e042c5ccd350bdf7ace638a872ff3631ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 01:55:24 -0300

    Saved meredith/__log__.py @2020-09-18 04:55

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -157,3 +157,21 @@ NameError: name 'img_direita' is not defined
     Caminho_direito.esquerda(Floresta)
 TypeError: 'SalaCenaNula' object is not callable
 '''},
+{'date': 'Fri Sep 18 2020 01:55:21.745 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    ambiente()
+  module <module> line 54
+    Caminho_direito.esquerda(Floresta)
+TypeError: 'SalaCenaNula' object is not callable
+'''},
