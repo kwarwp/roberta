36ce38d6ff54e9bd30b75a3a3e1f86952f541a40
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:12:44 -0300

    Created naomi/main.py @2020-09-21 23:12

---
+++ naomi/main.py
@@ -0,0 +1,12 @@
+# roberta.naomi.main.py
+"""     xxxxx
+
+.. codeauthor:: Angelica <.........@gmail.com>
+.. codeauthor:: Rodrigo Esquinelato <resquinelato@gmail.com>
+
+Changelog
+---------
+.. versionadded::    
+       - xxx
+
+"""
