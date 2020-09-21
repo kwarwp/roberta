6a0e69a3af305e669b951583eb043539807442dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 21:31:22 -0300

    Saved amanda/main.py @2020-09-21 00:31

--- amanda/main.py
+++ amanda/main.py
@@ -1,8 +1,18 @@
+
+
 """     xxxxx
 
-.. codeauthor:: Angélica <angelicazumpichiatti@yahoo.com.br>
+.. codeauthor:: Angelica Zumpichiatti <angelicazumpichiatti@yahoo.com.br>
+
 Changelog
 ---------
 .. versionadded::    
        - xxx
+"""     
+numero_um=1
+numero_dois=2
+letras = "Oi! Tudo bom?"
 
+operacao = numero_um + numero_dois
+
+print(operacao)
