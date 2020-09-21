adbfb6fe63b105cfb6a7ce548d2ba0043b04e6a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:14:11 -0300

    Saved naomi/__log__.py @2020-09-21 23:14

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -6,3 +6,10 @@
                                                                                       ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Sep 21 2020 20:14:11.881 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  #circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
+                                                                                      ^
+IndentationError: expected an indented block
+'''},
