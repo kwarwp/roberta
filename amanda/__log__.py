6687b224e909ca77259139c373065109a1e1555b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 21:21:30 -0300

    Saved amanda/__log__.py @2020-09-21 00:21

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -34,3 +34,10 @@ SyntaxError: invalid syntax : triple string end not found
      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Sep 20 2020 21:21:29.461 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  .. codeauthor:: Angelica <angelicazumpichiatti@yahoo.com.br>
+     ^
+SyntaxError: invalid syntax
+'''},
