67ac9831cfff373e95d87feb746eb3d65c0ec3f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:16:30 -0300

    Saved danae/__log__.py @2020-09-21 22:16

--- danae/__log__.py
+++ danae/__log__.py
@@ -78,3 +78,31 @@ IndentationError: expected an indented block
     c = [[solucao[ai] for ai in linha] for linha in Aldeia.KEYS]
 AttributeError: 'Aldeia' object has no attribute 'KEYS'
 '''},
+{'date': 'Mon Sep 21 2020 19:16:29.525 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    main()
+  module <module> line 21
+    circus(4, MASMORRA)
+  module circus.circus line 184
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 161
+    self.desafios[desafio](solucao)
+  module circus.circus line 134
+    self.desafio0(c)
+  module circus.circus line 126
+    c = [Piso(self.cena, 10*LADO+i*LADO, j*LADO, ai ) for j, linha in enumerate(solucao) for i, ai in enumerate(linha)]
+  module circus.circus line 33
+    az = azz[ai[1]]
+KeyError: E
+'''},
