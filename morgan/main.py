e138fc8bb85adb7fb4f8e48947817a8264c3bb7d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:30:18 -0300

    Saved morgan/main.py @2020-09-21 23:30

--- morgan/main.py
+++ morgan/main.py
@@ -1,1 +1,18 @@
 # roberta.morgan.main.py
+
+
+
+from circus.circus import circus 
+def desafio0():
+    TOPO_ESQUERDA = "AN"
+    TOPO_DIREITA= "AN"
+    TOPO_CENTRO= "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA = "AN", "AN","AN"
+    # O comando abaixo você vai entender no próximo desafio
+circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREIRA]]
+
+
+
+if _name_ == "_main_":
+desafio0()
