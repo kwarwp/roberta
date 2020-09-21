ac9570eea0ee9d944a2318efbbf63319311852a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:50:58 -0300

    Saved rachel/__log__.py @2020-09-21 23:50

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -236,3 +236,29 @@ KeyError: O
     self.elt.pos = pos[ai[0]]
 KeyError: O
 '''},
+{'date': 'Mon Sep 21 2020 20:50:56.814 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    desafio0()
+  module <module> line 12
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+  module circus.circus line 179
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 158
+    self.desafios[desafio](solucao)
+  module circus.circus line 123
+    c = [Piso(self.cena, 10*LADO+i*LADO, j*LADO, ai ) for j, linha in enumerate(solucao) for i, ai in enumerate(linha)]
+  module circus.circus line 35
+    self.elt.pos = pos[ai[0]]
+KeyError: O
+'''},
