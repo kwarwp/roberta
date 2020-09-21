a0f321e02d51e41656da5c4ab34f90f6c41227e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:02:27 -0300

    Saved sarah/__log__.py @2020-09-21 23:02

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -77,3 +77,19 @@ Exception: <SyntaxError: Unexpected token '{'>
 'error': '''
 Exception: <SyntaxError: Unexpected token '{'>
 '''},
+{'date': 'Mon Sep 21 2020 20:01:44.580 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    cena_fundo = Cena(MUNDO, esquerda=cena_esquerda, direita=cena_cireita)
+NameError: name 'cena_cireita' is not defined
+'''},
