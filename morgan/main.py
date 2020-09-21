b17bbbd1a227ffd23f90b3e64bd77dbdf40e604b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:45:27 -0300

    Saved morgan/main.py @2020-09-21 23:45

--- morgan/main.py
+++ morgan/main.py
@@ -7,13 +7,14 @@ def desafio0():
     TOPO_ESQUERDA = "LS"
     TOPO_DIREITA= "LO"
     TOPO_CENTRO= "JN"
-    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "JO", "FN", "AN"
     FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA = "AN", "AN","AN"
     # O comando abaixo você vai entender no próximo desafio
 
-    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREIRA]]
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], 
+              [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREIRA]])
 
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     desafio0()
