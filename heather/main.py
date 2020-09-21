4af77a720ce7e42f6acc92a8c8c0eea8b4973e85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:18:53 -0300

    Saved heather/main.py @2020-09-21 23:18

---
+++ heather/main.py
@@ -0,0 +1,29 @@
+# roberta.kristen.main.py
+"""     xxxxx
+
+.. codeauthor:: Lilian <lilianes93@gmail.com>
+
+Changelog
+---------
+.. versionadded::    
+       - xxx
+
+"""
+
+from circus.circus import circus
+def desafio0()
+    TOPO_ESQUERDA = "AN"
+    TOPO_DIREITA = "AN"
+    TOPO_CENTRO = "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+    # O comando abaixo voce vai entender no próximo desafio
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+
+
+# faça aqui a sua implementação do desafio
+if __name__ == "__main__":
+   desafio0()
+    #circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
