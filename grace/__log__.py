f14ce2011f9a26be4f049fd36e5cbc47a29f8e3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:36:54 -0300

    Saved grace/__log__.py @2020-09-21 22:36

--- grace/__log__.py
+++ grace/__log__.py
@@ -314,3 +314,19 @@ SyntaxError: invalid syntax
 'error': '''
 Exception: <TypeError: Cannot read property 'charAt' of undefined>
 '''},
+{'date': 'Mon Sep 21 2020 19:36:50.903 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    cena_esquerda.direita = Cena(MUNDO) 
+NameError: name 'cena_esquerda' is not defined
+'''},
