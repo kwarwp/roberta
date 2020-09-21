72e84793a33437a461cefff08b2a0548458c16b6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:33:09 -0300

    Saved ruzwana/main.py @2020-09-21 23:33

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -10,3 +10,6 @@ def desafio():
 # O comando abaixo voce vai entender no próximo desafio
 	circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
         MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+        
+        if __name__ == "__main__":
+desafio0()
