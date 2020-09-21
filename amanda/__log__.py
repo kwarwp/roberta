2ced77d1e91bfb5341afd031a2f86fbe7c03b652
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 21:51:20 -0300

    Saved amanda/__log__.py @2020-09-21 00:51

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -100,3 +100,19 @@ Traceback (most recent call last):
     operacao_soma = numero_1 + numero_2
 NameError: name 'numero_1' is not defined
 '''},
+{'date': 'Sun Sep 20 2020 21:51:19.924 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    print(operacao_soma)
+NameError: name 'operacao_soma' is not defined
+'''},
