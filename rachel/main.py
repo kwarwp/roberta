e6460565f7ef940f43ab3fa7de9bec662f7261f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:26:55 -0300

    Saved rachel/main.py @2020-09-21 23:26

--- rachel/main.py
+++ rachel/main.py
@@ -1,6 +1,14 @@
 # roberta.rachel.main.py
 
 from _spy.circus.circus import circus
-# faça aqui a sua implementação do desafio
-if "maria_clara" == "__main__": 
+
+TOPO_ESQUERDA = "AN"
+TOPO_DIREITA = "AN"
+TOPO_CENTRO = "AN"
+MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+# O comando abaixo voce vai entender no próximo desafio
+circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
 
