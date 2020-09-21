3e5d01efff88568a277cd1a9bf8280ba5f611622
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:31:08 -0300

    Saved kristen/__log__.py @2020-09-21 22:31

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -73,3 +73,19 @@ IndentationError: unexpected indent
     cenaFloresta.direita = CenaFloresta
 NameError: name 'CenaFloresta' is not defined
 '''},
+{'date': 'Mon Sep 21 2020 19:31:06.94 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 26
+    cenaBorboleta.direita = CenaFloresta
+NameError: name 'cenaBorboleta' is not defined
+'''},
