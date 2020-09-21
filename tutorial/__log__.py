72892825812948b7d44d6a0cc2cb3fb7d81c9c3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 15:06:36 -0300

    Saved tutorial/__log__.py @2020-09-21 18:06

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -481,3 +481,19 @@ NameError: name 'ambiente' is not defined
     desafio1()
 NameError: name 'desafio1' is not defined
 '''},
+{'date': 'Mon Sep 21 2020 18:06:34.959 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    desafio0()
+NameError: name 'desafio0' is not defined
+'''},
