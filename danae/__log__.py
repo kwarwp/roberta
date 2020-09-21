0f040f23850f7e02d19777ad0a36db231cc5d761
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:00:37 -0300

    Saved danae/__log__.py @2020-09-21 22:00

--- danae/__log__.py
+++ danae/__log__.py
@@ -54,3 +54,27 @@ AssertionError: AssertionError
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Sep 21 2020 19:00:36.583 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    main()
+  module <module> line 21
+    circus(4, MASMORRA)
+  module circus.circus line 179
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 158
+    self.desafios[desafio](solucao)
+  module circus.circus line 130
+    c = [[solucao[ai] for ai in linha] for linha in Aldeia.KEYS]
+AttributeError: 'Aldeia' object has no attribute 'KEYS'
+'''},
