2af839bbd41ceaec635af99f148ed2cca1d9e2fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:17:03 -0300

    Saved naomi/main.py @2020-09-21 23:17

--- naomi/main.py
+++ naomi/main.py
@@ -12,5 +12,17 @@ Changelog
 """
 
 from circus.circus import circus
-# faça aqui a sua implementação do desafio
-if __name__ == "__main__":#circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
+
+def desafio():
+    TOPO_ESQUERDA = "AN"
+    TOPO_DIREITA = "AN"
+    TOPO_CENTRO = "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+    # O comando abaixo voce vai entender no próximo desafio
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+            MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+
+if __name__ == "__main__":
+    desafio()
