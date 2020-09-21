68f93c2c92e24e0e78bc65ade876def39f7f72c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 18:49:11 -0300

    Saved tutorial/__log__.py @2020-09-21 21:49

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -788,3 +788,19 @@ IndentationError: expected an indented block
     from circus.circus import  circus
 ImportError: cannot import name 'circus'
 '''},
+{'date': 'Mon Sep 21 2020 21:49:10.668 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from circus.circus import circus
+ImportError: cannot import name 'circus'
+'''},
