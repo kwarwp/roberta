d4dd1db6bc1de81938c05d554bcde99e55a800ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:03:36 -0300

    Saved tutorial/quadro.py @2020-09-21 22:03

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -10,20 +10,3 @@ Changelog
        - xxx
 
 """
-#from patricia.danae.circus import Letra, Piso, Aldeia, circus
-from circus.circus import circus
-
-
-def desafio0():
-    TOPO_ESQUERDA = "AN"
-    TOPO_DIREITA = "AN"
-    TOPO_CENTRO = "AN"
-    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
-
-    # O comando abaixo voce vai entender no próximo desafio
-    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
-            MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
-
-if __name__ == "__main__":
-    desafio0()
