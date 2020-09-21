d49f2e5d42f15df41dfb2757fdb59f1be3dabc3d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:47:34 -0300

    Saved ruzwana/__log__.py @2020-09-21 23:47

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -131,3 +131,10 @@ SyntaxError: invalid syntax
     	circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Mon Sep 21 2020 20:47:32.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  circus(1,[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA],[MEIO_ESQUERDA, CENTRO,
+                                                                                                                                                  ^
+SyntaxError: invalid syntax
+'''},
