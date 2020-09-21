c41cfce8d73174d534fb0f63b31e05e0d8686ab0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 21:16:33 -0300

    Created amanda/__log__.py @2020-09-21 00:16

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Sep 20 2020 21:16:29.776 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 1
+  """     xxxxx
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    ^
+SyntaxError: invalid syntax : triple string end not found
+'''},
