68663c512fa9e10f9953e4c9733b0938fb03e055
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:01:40 -0300

    Saved sarah/__log__.py @2020-09-21 23:01

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -73,3 +73,7 @@ Exception: <SyntaxError: Unexpected token '{'>
 'error': '''
 Exception: <SyntaxError: Unexpected token '{'>
 '''},
+{'date': 'Mon Sep 21 2020 20:00:57.824 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: Unexpected token '{'>
+'''},
