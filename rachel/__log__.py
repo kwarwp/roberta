d441d43bf9295976150ff4ad27f01844816af004
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:24:47 -0300

    Created rachel/__log__.py @2020-09-21 23:24

---
+++ rachel/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 21 2020 20:24:42.403 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  
+  ^
+IndentationError: expected an indented block
+'''},
