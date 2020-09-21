e94462ebeda233e5e9fef4c8e92c094a7a25e140
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 19:54:40 -0300

    Saved sara/main.py @2020-09-21 22:54

--- sara/main.py
+++ sara/main.py
@@ -15,8 +15,14 @@ from _spy.vitollino.main  import Cena, STYLE
 STYLE["width"] = 900 # width = 300 (default)
 STYLE["heigth"] = "900px" # min-height = "300px"
 
-floresta= "http://1.bp.blogspot.com/-lGK-NZbMSBE/UUtrchW3_-I/AAAAAAAAIs4/hsWa_5_A-1g/s1600/floresta-amazonica.jpg"
+MUNDO= "https://img.elo7.com.br/product/zoom/2464D79/quadro-poster-mapa-mundi-bandeiras-quadro.jpg"
+ARCO= "http://nsrainha.com.br/content/uploads/Artigo-Arco-iris-20082018-1.jpg"
+LAPIS= "https://blog.grafittiartes.com.br/wp-content/uploads/2019/03/shutterstock_318381038.jpg"
+ELEMENTO= "https://www.ourolux.com.br/blog/wp-content/uploads/2019/06/acampar.jpg"
 
-floresta_amz= Cena(floresta)
+acamp_meio = Cena(ELEMENTO)
+arco_direito = Cena(ARCO)
+lapis_esquerda = Cena(LAPIS)
+nome_cena = Cena(MUNDO, esquerda=lapis_esquerda, direita = arco_direito, meio = acamp_meio)
 
-floresta_amz.vai()
+nome_cena.vai()
