99bb298b1ee48f9823803e58ac7ef2d09dabc122
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:39:01 -0300

    Saved ruzwana/main.py @2020-09-21 23:39

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -8,7 +8,7 @@ def desafio0():
 	FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
 
 # O comando abaixo voce vai entender no próximo desafio
-	circus(1, [TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+	circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
         MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
        
 if __name__ == "__main__":
