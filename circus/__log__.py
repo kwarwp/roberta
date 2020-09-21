dc1d4c34838c6baa5542296fee00fe61c9b7d957
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 18:46:33 -0300

    Created circus/__log__.py @2020-09-21 21:46

---
+++ circus/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 21 2020 21:46:31.551 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 216
+  
+  ^
+IndentationError: expected an indented block
+'''},
