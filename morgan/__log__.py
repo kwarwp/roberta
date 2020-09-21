f394eead253d086d54a743f52c95812f51797248
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:30:13 -0300

    Created morgan/__log__.py @2020-09-21 23:30

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 21 2020 20:30:11.350 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREIRA]]
+                                                                                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
