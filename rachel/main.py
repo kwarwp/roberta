bf093d482029310d485f2d9cb53044afe19c5f38
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:33:47 -0300

    Saved rachel/main.py @2020-09-21 23:33

--- rachel/main.py
+++ rachel/main.py
@@ -1,14 +1,16 @@
 # roberta.rachel.main.py
 
 from _spy.circus.circus import circus
-def desafio0()
-TOPO_ESQUERDA = "AN"
-TOPO_DIREITA = "AN"
-TOPO_CENTRO = "AN"
-MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+def desafio0():
+    TOPO_ESQUERDA = "LS"
+    TOPO_DIREITA = "LO"
+    TOPO_CENTRO = "JN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
 
-# O comando abaixo voce vai entender no próximo desafio
-circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
-        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+    # O comando abaixo voce vai entender no próximo desafio
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+            MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
 
+if __name__ == "__main__":
+    desafio0()
