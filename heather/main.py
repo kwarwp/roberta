5109e217d5df5e01379a8301d4b6c2b09dda8128
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:53:32 -0300

    Saved heather/main.py @2020-09-21 23:53

--- heather/main.py
+++ heather/main.py
@@ -22,8 +22,15 @@ def desafio0():
     circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
         MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
 
+    MASMORRA = [[ "AN", "AN", "AN", "AN", "AN", "AN"],
+                [ "AN", "AN", "AN", "AN", "AN", "AN"],
+                [ "AN", "AN", "AN", "AN", "AN", "AN"],
+                [ "AN", "AN", "AN", "AN", "AN", "AN"],
+                [ "AN", "AN", "AN", "AN", "AN", "AN"]
+                ]
 
+    circus(2, MASMORRA)
 # faça aqui a sua implementação do desafio
 if __name__ == "__main__":
-    desafio0()
+    desafio1()
     #circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
