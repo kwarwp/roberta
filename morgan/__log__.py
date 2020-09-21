eed2f2ce6c60390054a3e295ed697f36405eec24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:43:37 -0300

    Saved morgan/__log__.py @2020-09-21 23:43

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 21 2020 20:43:36.963 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO, MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREIRA]]
+                                                                                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
