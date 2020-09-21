f5916075ce3921ea2e641d2c2ce063f4cfdff8ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 20:35:07 -0300

    Saved heather/main.py @2020-09-21 23:35

--- heather/main.py
+++ heather/main.py
@@ -15,7 +15,7 @@ def desafio0():
     TOPO_ESQUERDA = "LS"
     TOPO_DIREITA = "LO"
     TOPO_CENTRO = "JN"
-    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "JO", "FN", "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "JO", "FN", "JL"
     FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
 
     # O comando abaixo voce vai entender no próximo desafio
