5a8ec889106db2fc0899d7cb592d28035ec11e1b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-22 14:01:30 -0300

    Saved naomi/__log__.py @2020-09-22 17:01

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -62,3 +62,29 @@ NameError: name 'MASMORRA' is not defined
     az = azz[ai[1]]
 KeyError: D
 '''},
+{'date': 'Tue Sep 22 2020 14:01:28.924 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    desafio2()
+  module <module> line 35
+    circus(2, MASMORRA)
+  module circus.circus line 179
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 158
+    self.desafios[desafio](solucao)
+  module circus.circus line 123
+    c = [Piso(self.cena, 10*LADO+i*LADO, j*LADO, ai ) for j, linha in enumerate(solucao) for i, ai in enumerate(linha)]
+  module circus.circus line 33
+    az = azz[ai[1]]
+KeyError: D
+'''},
