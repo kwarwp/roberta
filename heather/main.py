c1d2c7dd78e8ff43c2d1ac9ba9261d9b3c87130a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 21:05:20 -0300

    Saved heather/main.py @2020-09-22 00:05

--- heather/main.py
+++ heather/main.py
@@ -25,7 +25,7 @@ def desafio0():
 def desafio1():
     MASMORRA = [[ "LS", "JN", "HN", "JN", "HN", "KO"],
                 [ "HO", "AN", "FN", "FN", "BN", "IL"],
-                [ "JO", "DS", "AN", "AN", "AN", "AN"],
+                [ "JO", "DS", "ON", "CS", "DS", "JL"],
                 [ "AN", "AN", "AN", "AN", "AN", "AN"],
                 [ "AN", "AN", "AN", "AN", "AN", "AN"]
                 ]
