0226b08090e6610fae61abc84e7505672b04ce38
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-23 11:04:15 -0300

    Saved naomi/__log__.py @2020-09-23 14:04

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -486,3 +486,29 @@ AttributeError: 'Aldeia' object has no attribute 'KEYS'
     c = [[solucao[ai] for ai in linha] for linha in Aldeia.KEYS]
 AttributeError: 'Aldeia' object has no attribute 'KEYS'
 '''},
+{'date': 'Wed Sep 23 2020 11:04:13.910 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    desafio5()
+  module <module> line 55
+    circus(5, MASMORRA)
+  module circus.circus line 179
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 158
+    self.desafios[desafio](solucao)
+  module circus.circus line 135
+    self.desafio2(solucao)
+  module circus.circus line 130
+    c = [[solucao[ai] for ai in linha] for linha in Aldeia.KEYS]
+AttributeError: 'Aldeia' object has no attribute 'KEYS'
+'''},
