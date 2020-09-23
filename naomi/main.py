e81c769eb54b2beaa4ee2c68b0ce85ca37ce7292
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-23 09:21:42 -0300

    Saved naomi/main.py @2020-09-23 12:21

--- naomi/main.py
+++ naomi/main.py
@@ -41,8 +41,8 @@ def desafio3():
     circus(3, MASMORRA)
     
 def desafio4():
-    MASMORRA = {'Cahuitz': 'LO', 'Cauha': 'LS', 'Coycol': 'AN',
-                'Huatlya': 'AN', 'Micpe': 'AN', 'Nenea': 'AN',
+    MASMORRA = {'Cahuitz': 'LO', 'Cauha': 'LS', 'Coycol': 'JS',
+                'Huatlya': 'JL', 'Micpe': 'GS', 'Nenea': 'JN',
                 'Pallotl': 'AN', 'Tetlah': 'AN', 'Zitllo': 'AN'}
     circus(4, MASMORRA)
 
