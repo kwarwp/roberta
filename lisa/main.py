6a7cf858efb87381619db6383bc44293c95c9057
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 19:29:39 -0300

    Saved lisa/main.py @2020-09-25 22:29

--- lisa/main.py
+++ lisa/main.py
@@ -1,7 +1,5 @@
 # roberta.lisa.main.py
-
 """     xxxxx
-
 .. codeauthor:: Luiza <luizaanjo2007@gmail.com>
 .. codeauthor:: Rodrigo Esquinelato <resquinelato@gmail.com>
 
@@ -11,11 +9,22 @@ Changelog
        - xxx
 
 """
-#NSLO (0, 2, 1, -1)
-from circus.circus import circus
 
-TOPO_ESQUERDA = "AN"
-TOPO_DIREITA = "AN"
-TOPO_CENTRO = "AN"
-MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+from circus.circus import circus
+def desafio0():
+    TOPO_ESQUERDA = "AN"
+    TOPO_DIREITA = "AN"
+    TOPO_CENTRO = "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+    
+    # O comando abaixo voce vai entender no próximo desafio
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+        MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+        
+        
+        # faça aqui a sua implementação do desafio
+if __name__ == "__main__":
+    desafio0()
+    #circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
+        
