77e44b33c63293b189fa263abac9c5f61fa169a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 19:18:28 -0300

    Created lisa/__log__.py @2020-09-25 22:18

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Sep 25 2020 19:18:17.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  TOPO_ESQUERDA = "AN"
+  ^
+IndentationError: expected an indented block
+'''},
