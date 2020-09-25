daa7dab82bbc096dd868ec11096d20ea2bc39a44
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 19:17:30 -0300

    Created lisa/main.py @2020-09-25 22:17

---
+++ lisa/main.py
@@ -0,0 +1,21 @@
+# roberta.lisa.main.py
+
+"""     xxxxx
+
+.. codeauthor:: Luiza <luizaanjo2007@gmail.com>
+.. codeauthor:: Rodrigo Esquinelato <resquinelato@gmail.com>
+
+Changelog
+---------
+.. versionadded::    
+       - xxx
+
+"""
+#NSLO (0, 2, 1, -1)
+from circus.circus import circus
+
+TOPO_ESQUERDA = "AN"
+TOPO_DIREITA = "AN"
+TOPO_CENTRO = "AN"
+MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
