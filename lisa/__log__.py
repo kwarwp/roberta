5fa3357ed3aba38bb8a17539ad182766fbba176a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 20:56:48 -0300

    Saved lisa/__log__.py @2020-09-25 23:56

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -240,3 +240,19 @@ NameError: name 'MASMORRA' is not defined
     circus(3, MASMORRA)
 NameError: name 'MASMORRA' is not defined
 '''},
+{'date': 'Fri Sep 25 2020 20:56:39.137 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    circus(3, MASMORRA)
+NameError: name 'MASMORRA' is not defined
+'''},
