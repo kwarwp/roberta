4c3e013e5d51a5f04f51198d974e739768f486da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 19:23:42 -0300

    Saved lisa/__log__.py @2020-09-25 22:23

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -13,3 +13,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Sep 25 2020 19:23:33.220 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  if __name__ == "__main__" :
+  ^
+IndentationError: unexpected indent
+'''},
