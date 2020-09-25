0443bec165b5f1dd3fc4af3ee60c9ca2e4261074
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 20:54:21 -0300

    Saved lisa/__log__.py @2020-09-25 23:54

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -194,3 +194,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Sep 25 2020 20:54:12.385 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 45
+  MASMORRA = {'Cahuitz': 'AN', 'Cauha': 'AN', 'Coycol': 'AN',
+  ^
+IndentationError: expected an indented block
+'''},
