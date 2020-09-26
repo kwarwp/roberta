519e7e89ac3be36e9b847ee791322124298839c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 21:56:00 -0300

    Saved lisa/__log__.py @2020-09-26 00:55

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -407,3 +407,19 @@ IndentationError: expected an indented block
              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Sep 25 2020 21:55:49.640 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+TypeError: 'module' object is not callable
+'''},
