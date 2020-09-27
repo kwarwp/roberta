76c1444e74049395bc838081c56342e4c678ad10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 13:56:07 -0300

    Saved heather/main.py @2020-09-27 16:56

--- heather/main.py
+++ heather/main.py
@@ -32,7 +32,7 @@ def desafio1():
 
 def desafio2():
     MASMORRA = {'Cahuitz': 'LS', 'Cauha': 'JN', 'Coycol': 'LS',
-                'Huatlya': 'JO', 'Micpe': 'FN', 'Nenea': 'FN',
+                'Huatlya': 'JO', 'Micpe': 'FN', 'Nenea': 'GL',
                 'Pallotl': 'GO', 'Tetlah': 'LO', 'Zitllo': 'GL'}
 
 
