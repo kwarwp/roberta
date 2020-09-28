0fa31a268d6306edc1d4e3dabcefd53260ffa7ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 19:59:30 -0300

    Saved danae/__log__.py @2020-09-28 22:59

--- danae/__log__.py
+++ danae/__log__.py
@@ -214,3 +214,25 @@ TypeError: 'NoneType' object is not iterable
     az = azz[ai[1]]
 KeyError: E
 '''},
+{'date': 'Mon Sep 28 2020 19:59:29.457 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    main()
+  module <module> line 21
+    circus(8, MASMORRA)
+  module circus.circus line 181
+    Aldeia(Jogo()).circus(desafio, solucao, desafio)
+  module circus.circus line 159
+    self.desafios[desafio](solucao)
+IndexError: list index out of range
+'''},
