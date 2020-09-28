7ec8b5886e8b27e355692b07743b5bfa7214a1d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 18:55:30 -0300

    Saved tutorial/quadro.py @2020-09-28 21:55

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -10,29 +10,3 @@ Changelog
        - xxx
 
 """
-from circus.circus import circus
-
-def desafio0():
-    TOPO_ESQUERDA = "AN"
-    TOPO_DIREITA = "AN"
-    TOPO_CENTRO = "AN"
-    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
-
-    # O comando abaixo voce vai entender no próximo desafio
-    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
-            MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
-            
-def desafio1():
-    MASMORRA = [[ "AN", "AN", "AN", "AN", "AN", "AN"],
-                [ "AN", "AN", "AN", "AN", "AN", "AN"],
-                [ "AN", "AN", "AN", "AN", "AN", "AN"],
-                [ "AN", "AN", "AN", "AN", "AN", "AN"],
-                [ "AN", "AN", "AN", "AN", "AN", "AN"]
-                ]
-
-    circus(2, MASMORRA,2)
-
-
-if __name__ == "__main__":
-    desafio1()
