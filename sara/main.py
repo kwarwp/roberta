8135f5483e15ffb2b362d88098113b6e69818062
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:16:13 -0300

    Saved sara/main.py @2020-09-28 23:16

--- sara/main.py
+++ sara/main.py
@@ -28,11 +28,23 @@ Changelog
 
 #nome_cena.vai()
 
-from _spy.vitollino.main  import Cena, STYLE
+#from _spy.vitollino.main  import Cena, STYLE
 
-VARIAVEL = "http://1.bp.blogspot.com/-lGK-NZbMSBE/UUtrchW3_-I/AAAAAAAAIs4/hsWa_5_A-1g/s1600/floresta-amazonica.jpg"
+#VARIAVEL = "http://1.bp.blogspot.com/-lGK-NZbMSBE/UUtrchW3_-I/AAAAAAAAIs4/hsWa_5_A-1g/s1600/floresta-amazonica.jpg"
 
-def ambiente():
-	floresta = Cena(VARIAVEL)
-	floresta.vai()
-ambiente()
+#def ambiente():
+#	floresta = Cena(VARIAVEL)
+#	floresta.vai()
+#ambiente()
+
+numero = 3
+
+def verifica():
+	if numero > 5:
+    	print ("Número é maior que cinco")
+    elif numero = 5:
+    	print ("Igualdade")
+    else numero < 5:
+    	print ("Número é maior que cinco")
+
+verifica()
