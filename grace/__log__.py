972c3d31f14cd3e4393f3b319eeea51f590cf0a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:19:19 -0300

    Saved grace/__log__.py @2020-09-28 23:19

--- grace/__log__.py
+++ grace/__log__.py
@@ -461,3 +461,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Sep 28 2020 20:19:18.491 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 67
+  print("num é maior que 16")
+  ^
+IndentationError: expected an indented block
+'''},
