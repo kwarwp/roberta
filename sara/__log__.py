d384d19a45cd49efdf60d7769e33604e018cb274
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:19:06 -0300

    Saved sara/__log__.py @2020-09-28 23:19

--- sara/__log__.py
+++ sara/__log__.py
@@ -109,3 +109,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Sep 28 2020 20:19:05.630 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 44
+  print ("Número é maior que cinco")
+  ^
+IndentationError: expected an indented block
+'''},
