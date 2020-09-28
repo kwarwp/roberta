67b7180bad399693691d5d57757898f66632f046
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:05:01 -0300

    Saved kristen/main.py @2020-09-28 23:05

--- kristen/main.py
+++ kristen/main.py
@@ -9,32 +9,15 @@ Changelog
        - xxx
 
 """
+from _spy.vitollino.main import Cena
 
-from _spy.vitollino.main import Cena, STYLE
+VARIAVEL_GLOBAL = "https://i.pinimg.com/736x/f5/02/0d/f5020d2e11b0f570ea3087ce7f54c435.jpg"
 
-STYLE ["width"] = 600
-STYLE ["height"] = 600
 
-FLORESTA = "https://image.freepik.com/vetores-gratis/folhagem-de-floresta-tropical-paisagem_23-2148271243.jpg"
-BORBOLETA = "https://store-images.s-microsoft.com/image/apps.2544.13768621950225582.167ba0c8-6eb8-47bb-96fe-278c89bf0dc9.ea440c13-fd1d-4705-b62c-9bfd9054b8b3?w=672&h=378&q=80&mode=letterbox&background=%23FFE4E4E4&format=jpg"
-INDIO = "https://i.imgur.com/DUGXbFb.png"
-OCA = "https://i.imgur.com/JzoV9GT.png"
 
-#cenaFloresta = Cena(img = FLORESTA)
+def ambiente ():
+    floresta = Cena(VARIAVEL)
+    floreta.vai()
 
-nome_da_cena_meio = Cena(INDIO)
-nome_da_cena_direita = Cena(BORBOLETA)
-nome_da_cena_esquerda = Cena(OCA)
-nome_da_cena = Cena(FLORESTA, esquerda=nome_da_cena_esquerda, direita=nome_da_cena_direita, meio=nome_da_cena_meio)
 
-nome_da_cena_esquerda.direita = nome_da_cena
-nome_da_cena_esquerda.esquerda = nome_da_cena
-#nome_da_cena_esquerda.esquerda = cenaFloresta
-
-nome_da_cena_direita.direita = nome_da_cena
-nome_da_cena_direita.esquerda = nome_da_cena
-
-nome_da_cena_meio.esquerda = nome_da_cena
-nome_da_cena_meio.direita = nome_da_cena
-
-nome_da_cena.vai()
+ambiente()
