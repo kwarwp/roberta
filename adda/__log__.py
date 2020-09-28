5c5fe61b77946ac1c0cc438a6c530fa15d039a3f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:00:06 -0300

    Saved adda/__log__.py @2020-09-28 23:00

--- adda/__log__.py
+++ adda/__log__.py
@@ -174,3 +174,21 @@ NameError: name 'cenaMUNDO' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Sep 28 2020 20:00:01.367 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    ambiente()
+  module <module> line 46
+    floresta = Cena(VARIAVEL)
+UnboundLocalError: local variable 'VARIAVEL' referenced before assignment
+'''},
