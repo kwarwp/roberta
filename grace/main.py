c1c6f5216ed6ad7b214c7db0cb9621d8ca377cbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 19:58:53 -0300

    Saved grace/main.py @2020-09-28 22:58

--- grace/main.py
+++ grace/main.py
@@ -10,17 +10,17 @@ Changelog
 """
 
 
-from _spy.vitollino.main import Cena,STYLE
+#from _spy.vitollino.main import Cena,STYLE
 
 
 
-STYLE["width"] = 900 # width = 300 (default)
-STYLE["heigth"] = "900px" # min-height = "300px"
+#STYLE["width"] = 900 # width = 300 (default)
+#STYLE["heigth"] = "900px" # min-height = "300px"
 
-FLORESTA= 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia24412/floresta-cpt.jpg'
-MUNDO= 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png'
-ARCO= 'https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg'
-LAPIS= "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg"
+#FLORESTA= 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia24412/floresta-cpt.jpg'
+#MUNDO= 'https://www.estadosecapitaisdobrasil.com/wp-content/uploads/2015/04/mapa-mundi.png'
+#ARCO= 'https://static.vecteezy.com/system/resources/previews/000/292/669/non_2x/vector-a-beautiful-rainbow-on-white-background.jpg'
+#LAPIS= "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-onda-colorida-D_NQ_NP_913239-MLB31144572917_062019-F.jpg"
 
 #floresta= Cena(FLORESTA) 
 #mundo= Cena(MUNDO)
@@ -29,18 +29,40 @@ LAPIS= "https://http2.mlstatic.com/adesivo-parede-quarto-infantil-lapis-de-cor-o
 
 #floresta_verde - cena(floresta, direita - mundo, esquerda - arco, meio - lapis) 
 
-def ambiente():
-    cena_mundo = Cena(MUNDO)
-    cena_arco = Cena(ARCO)
-    cena_lapis = Cena(LAPIS)
+#def ambiente():
+    #cena_mundo = Cena(MUNDO)
+    #cena_arco = Cena(ARCO)
+    #cena_lapis = Cena(LAPIS)
     
-    floresta_verde = Cena(FLORESTA, direita = cena_mundo , esquerda = cena_arco , meio = cena_lapis )
-    cena_mundo.esquerda = cena_lapis
+    #floresta_verde = Cena(FLORESTA, direita = cena_mundo , esquerda = cena_arco , meio = cena_lapis )
+    #cena_mundo.esquerda = cena_lapis
     #mundo = Cena(MUNDO, esquerda = floresta_verde) 
-    floresta_verde.vai()
+    #floresta_verde.vai()
     #floresta_verde.LAPIS = Cena(MUNDO) 
     
+#ambiente()
+
+#.. codeauthor:: mariaclara < mari4cla@gmail.com >
+
+#Changelog
+#---------
+#.. versionadded::    
+#       - xxx
+#"""
+
+
+from _spy.vitollino.main import Cena,STYLE 
+
+VARIA: "https://macmagazine.uol.com.br/wp-content/uploads/2015/04/16-floresta.jpg"
+
+def ambiente(): 
+    floresta = Cena(VARIA)
+    floresta.vai
+    
 ambiente()
+
+
+
 
 
 
