4d3df8c0388492dcf3efb38cba386dd362d29025
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 00:34:22 -0300

    Saved ruzwana/main.py @2020-09-28 03:34

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,15 +1,14 @@
 # roberta.ruzwana.main.py
 from circus.circus import circus
-def desafio0():
-	TOPO_ESQUERDA = "LS"
-	TOPO_DIREITA = "LO"
-	TOPO_CENTRO = "JN"
-	MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
-	FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+TOPO_ESQUERDA = "AN"
+TOPO_DIREITA = "AN"
+TOPO_CENTRO = "AN"
+MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
 
 # O comando abaixo voce vai entender no próximo desafio
-        circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
         MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
-       
 if __name__ == "__main__":
 	desafio0()
