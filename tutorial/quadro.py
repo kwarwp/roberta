ec9aae2db56ab3fe736e39685222e3c33fb5fc22
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:31:53 -0300

    Saved tutorial/quadro.py @2020-09-28 23:31

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -10,19 +10,22 @@ Changelog
        - xxx
 
 """
-
-numero=3
+numero=5
 
 def verifica():
     if numero > 5:
         print("Numero é maior que cinco")
-    elif numero = 5:
+    elif numero == 5:
         print("Opa! Igualdade")
     else:
         print("Numero não é maior que cinco")
-        
-verifica()
+#verifica()
 
+animais = ["macaco","águia","pardal","leão", "cachorro"]
 
-
+for x in animais:
+    #print(x)
+    if x == "leão":
+        break
+    print(x)
 
