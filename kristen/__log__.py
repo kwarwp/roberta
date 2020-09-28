b368aa2ef6cd1786222fdaec64847236e242133b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:05:07 -0300

    Saved kristen/__log__.py @2020-09-28 23:05

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -153,3 +153,21 @@ NameError: name 'cena_esquerda' is not defined
     cena_esquerda.direita = cenaFloresta
 NameError: name 'cena_esquerda' is not defined
 '''},
+{'date': 'Mon Sep 28 2020 20:05:05.396 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    ambiente()
+  module <module> line 19
+    floresta = Cena(VARIAVEL)
+NameError: name 'VARIAVEL' is not defined
+'''},
