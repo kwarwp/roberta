46a01266ba762e87ba7ce5e6bf5a9a6730d9272e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-28 20:32:12 -0300

    Saved libby/__log__.py @2020-09-28 23:32

--- libby/__log__.py
+++ libby/__log__.py
@@ -54,3 +54,22 @@ NameError: name 'gato' is not defined
     floresta_verde= Cena(FLORESTA, direita= lemori, esquerda= cachorro, meio = gato)
 NameError: name 'gato' is not defined
 '''},
+{'date': 'Mon Sep 28 2020 20:32:11.167 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''macaco
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    time.sleep(1)
+  module time line 223
+    raise NotImplementedError("Blocking functions like time.sleep() are not "
+NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
+'''},
