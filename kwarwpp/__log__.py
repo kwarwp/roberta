1b28fda6ed9434f0e251f2ee232c9fc9d6949afd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 13:29:09 -0300

    Saved kwarwpp/__log__.py @2020-09-30 16:29

--- kwarwpp/__log__.py
+++ kwarwpp/__log__.py
@@ -203,3 +203,21 @@ TypeError: 'undefined' object is not iterable
     for elem in iterable:
 TypeError: 'undefined' object is not iterable
 '''},
+{'date': 'Wed Sep 30 2020 13:26:50.417 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    main(Jogo, STYLE)
+  module <module> line 42
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(self.Indio))
+AttributeError: 'GUI' object has no attribute 'Indio'
+'''},
