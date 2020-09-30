0ffe22aa4487a868a5c3c7935a58b2ef45abf647
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 13:33:08 -0300

    Saved kwarwpp/__log__.py @2020-09-30 16:33

--- kwarwpp/__log__.py
+++ kwarwpp/__log__.py
@@ -239,3 +239,21 @@ AttributeError: 'GUI' object has no attribute 'Indio'
     return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Índio, India))
 NameError: name 'Índio' is not defined
 '''},
+{'date': 'Wed Sep 30 2020 13:30:50.296 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    main(Jogo, STYLE)
+  module <module> line 42
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Índio , Kaiowa))
+NameError: name 'Índio' is not defined
+'''},
