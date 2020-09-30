ec99c532dcf8c5f8993d6af5749cf6237ab929e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 14:23:28 -0300

    Saved kwarwpp/__log__.py @2020-09-30 17:23

--- kwarwpp/__log__.py
+++ kwarwpp/__log__.py
@@ -299,3 +299,19 @@ TypeError: 'undefined' object is not iterable
     def main(vitollino, medidas={}, mapa=None, indios=(*kwargs)):
 NameError: name 'kwargs' is not defined
 '''},
+{'date': 'Wed Sep 30 2020 14:21:10.159 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from kwarwp.kwarwpp import main as kwarwp_main, Indio
+ImportError: cannot import name 'main'
+'''},
