5838e3d3c07702756dc1ce729c78af9032b593c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 19:16:07 -0300

    Saved kwarwpp/__log__.py @2020-09-30 22:16

--- kwarwpp/__log__.py
+++ kwarwpp/__log__.py
@@ -322,3 +322,40 @@ ImportError: cannot import name 'main'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 30 2020 19:13:47.949 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''False False <function _pos>
+False False <function _pos>
+True False <function _pos>
+False False <function _pos>
+False False <function _pos>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    main(Jogo, STYLE)
+  module <module> line 38
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Indio, Indio))
+  module kwarwp.kwarwpp line 600
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=mapa, indios=indios)
+  module kwarwp.kwarwpp line 424
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module kwarwp.kwarwpp line 460
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module kwarwp.kwarwpp line 514
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa, taba=self)
+  module kwarwp.kwarwppart line 70
+    self.acessa(ocupante)
+  module kwarwp.kwarwppart line 122
+    ocupante.ocupa(self)
+  module kwarwp.kwarwppart line 339
+    self.empurrante.ocupa(self.vaga) if self.empurrante is not NULO else None
+AttributeError: 'Tora' object has no attribute 'empurrante'
+'''},
