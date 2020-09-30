dc0f7ebafdf0c515ea5272b06642538a1610ec2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 12:43:18 -0300

    Created kwarwp/main.py @2020-09-30 15:43

---
+++ kwarwp/main.py
@@ -0,0 +1,46 @@
+# roberta.kwarwp.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Jogo para ensino de programação Python.
+
+.. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
+
+Changelog
+---------
+.. versionadded::    20.07
+        classe Vitollino.
+
+"""
+from _spy.vitollino.main import Jogo, STYLE
+from kwarwp.kwarapp import main as kwarwp_main, Indio
+MAPA_INICIO = """
+@....&
+......
+.....#
+.#.p`.
+"""
+
+class Kaiowa(Indio):
+    def executa(self):
+        """ Roteiro do índio. Conjunto de comandos para ele executar.
+        """
+        self.anda()
+        self.direita()
+        self.pega()
+        self.esquerda()
+        self.anda()
+        self.anda()
+        self.anda()
+        self.direita()
+        self.larga()
+
+
+class Vitollino(Jogo):
+    """ Empacota o engenho de jogo Vitollino """
+    pass
+
+def main(vitollino, medidas):
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Indio, Kaiowa))
+        
+    
+if __name__ == "__main__":
+    main(Jogo, STYLE)
