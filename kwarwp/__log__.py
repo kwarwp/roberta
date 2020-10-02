c1d205b8243138ad4854ad94abf9e079fec511cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-02 19:52:22 -0300

    Saved kwarwp/__log__.py @2020-10-02 22:52

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -69,3 +69,27 @@ Traceback (most recent call last):
     self.empurrante.ocupa(self.vaga) if self.empurrante is not NULO else None
 AttributeError: 'Tora' object has no attribute 'empurrante'
 '''},
+{'date': 'Fri Oct 02 2020 19:52:21.542 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    main(Jogo, STYLE)
+  module <module> line 42
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Kaiowa))
+  module kwarwp.kwarwpp line 600
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=mapa, indios=indios)
+  module kwarwp.kwarwpp line 423
+    self.indios = deque(indios or [Indio])
+  module _collections line 48
+    for elem in iterable:
+TypeError: 'undefined' object is not iterable
+'''},
