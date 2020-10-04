460e44d1a325853e3f6b21b4a8d083d279289788
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-03 23:05:10 -0300

    Saved amanda/__log__.py @2020-10-04 02:05

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -273,3 +273,10 @@ SyntaxError: invalid syntax
        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Oct 03 2020 23:05:09.141 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 92
+  if x=="leão"
+               ^
+SyntaxError: invalid syntax
+'''},
