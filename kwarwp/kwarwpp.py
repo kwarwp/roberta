bff4a40ffe3f39939ce321ab1a750d23c6727ebb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 22:43:42 -0300

    Saved kwarwp/kwarwpp.py @2020-10-05 01:43

--- kwarwp/kwarwpp.py
+++ kwarwp/kwarwpp.py
@@ -445,7 +445,7 @@ class Kwarwp():
         "@": Fab(self.barra, f"{IMGUR}tLLVjfN.png"), # PICHE
         "~": Fab(self.coisa, f"{IMGUR}UAETaiP.gif"), # CEU
         "*": Fab(self.coisa, f"{IMGUR}PfodQmT.gif"), # SOL
-        "|": Fab(self.coisa, f"{IMGUR}uwYPNlz.png")  # CERCA
+        "|": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"),  # CERCA
         "%": Fab(self.coisa, f"{IMGUR}prAN5mi.png")  #CERCA_ROCHOSA
         }
         """Dicionário que define o tipo e a imagem do objeto para cada elemento."""
