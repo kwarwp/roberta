9802dcaf2bfe0ef244336e807bd88aa7df194e3d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 15:28:05 -0300

    Saved kwarwp/main.py @2020-10-05 18:28

--- kwarwp/main.py
+++ kwarwp/main.py
@@ -85,7 +85,6 @@ class Apo(Indio):
             CONSELHO DO PAGÉ:
                "Não tente levantar a pedra. Faz mal pro umbigo"
         """
-        self.pega()
         self.empurra()
         
         
