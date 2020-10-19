bbe285fa3127737d3cf24bb9ae2570a968eda469
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-19 19:55:25 -0300

    Created kellee/__log__.py @2020-10-19 22:55

---
+++ kellee/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Mon Oct 19 2020 19:55:22.305 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    diga_o_nome()           #observe como a função é chamada.
+TypeError: diga_o_nome() missing 1 positional argument: nome
+'''},
