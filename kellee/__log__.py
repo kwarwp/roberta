eff96b4fc6b15c1bc3080bc279bd93954bb96d44
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-19 20:01:04 -0300

    Saved kellee/__log__.py @2020-10-19 23:01

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -65,3 +65,19 @@ TypeError: diga_o_nome() missing 1 positional argument: nome
     nome = Gabriela    # observe que na ausência de parâmetros alguns valores precisam ser declarados
 NameError: name 'Gabriela' is not defined
 '''},
+{'date': 'Mon Oct 19 2020 20:01:03.712 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    print(soma(n1, n2))
+NameError: name 'n1' is not defined
+'''},
