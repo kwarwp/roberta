3a87fa45577c20e39518cc323e5311f0fd4c2e15
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-24 19:45:01 -0300

    Saved lisa/__log__.py @2020-10-24 22:45

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -619,3 +619,10 @@ SyntaxError: invalid syntax
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Oct 24 2020 19:44:21.735 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  elif numero = 10:
+               ^
+SyntaxError: invalid syntax
+'''},
