cfdc5f9117393627f1cc4ad325db75587350521a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-24 20:35:24 -0300

    Saved lisa/__log__.py @2020-10-24 23:35

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -689,3 +689,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Oct 24 2020 20:34:45.71 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 28
+  if x == "gato":
+  ^
+IndentationError: unexpected indent
+'''},
