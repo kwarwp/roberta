233fa9405d0f10198f33e075aabe8bfe030e4a7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-25 04:47:08 -0300

    Saved lisa/__log__.py @2020-10-25 07:47

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -828,3 +828,10 @@ IndexError: list index out of range
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Oct 25 2020 04:46:25.918 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  print("O céu é {0}, a flor é {1}, meu carro é {2}".format(cor1, cor2))
+  ^
+IndentationError: unexpected indent
+'''},
