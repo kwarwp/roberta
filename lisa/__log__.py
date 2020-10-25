106be856559773892cd53d687c091a98fdfb1f91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-25 04:20:11 -0300

    Saved lisa/__log__.py @2020-10-25 07:20

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -724,3 +724,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Oct 25 2020 04:19:31.344 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 37
+  print(nome):
+              ^
+SyntaxError: invalid syntax
+'''},
