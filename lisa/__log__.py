361e687f5ca4d0407ab068d613861677ccbbdcd5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-25 04:46:35 -0300

    Saved lisa/__log__.py @2020-10-25 07:46

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -805,3 +805,19 @@ IndexError: list index out of range
     print = ("O céu é {}, a flor é {}, meu carro é {}".format(cor1, cor2))
 IndexError: list index out of range
 '''},
+{'date': 'Sun Oct 25 2020 04:45:50.701 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    print("O céu é {0}, a flor é {1}, meu carro é {2}".format(cor1, cor2))
+IndexError: list index out of range
+'''},
