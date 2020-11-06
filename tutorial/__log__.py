efba5e111d798c1f71837b081896788f214c41ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 18:57:42 -0300

    Saved tutorial/__log__.py @2020-11-06 21:57

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1173,3 +1173,19 @@ NameError: name 'chama' is not defined
     texto_ = Texto(nome_da_cena, txt = "Mensagem desejada",foi=chama)
 NameError: name 'chama' is not defined
 '''},
+{'date': 'Fri Nov 06 2020 18:57:41.437 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    texto_ = Texto(nome_da_cena, txt = "Mensagem desejada",foi = chama)
+NameError: name 'chama' is not defined
+'''},
