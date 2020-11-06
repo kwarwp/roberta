531105b9c324f1fafd3f8a0f56d9da092999e4f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 19:34:53 -0300

    Saved tutorial/quadro.py @2020-11-06 22:34

--- tutorial/quadro.py
+++ tutorial/quadro.py
@@ -15,8 +15,6 @@ from _spy.vitollino.main import Cena, STYLE, Texto
 
 """ O objeto é o elementoclicável de alguma cena.
 """
-FUNDO =
-LIVRO = "https://comunicamack.files.wordpress.com/2016/12/livro.png"
 
 STYLE["width"] = 900 # width = 300 (default) 
 STYLE["heigth"] = "900px" # min-height = "300px
@@ -24,15 +22,17 @@ STYLE["heigth"] = "900px" # min-height = "300px
 
 MINHA_CENA =  "https://img.elo7.com.br/product/original/1D27E33/painel-cenario-mundo-encantado-frete-gratis-cenario.jpg"
    
+def resultado(l):
+    r = Texto(nome_da_cena, txt=dicionario[l])
+    r.vai()   
+
 nome_da_cena = Cena(MINHA_CENA)
 nome_da_cena.vai()
    
 dicionario = dict(A=10, B=20)
-texto = Texto(nome_da_cena, txt = "escolha", foi = foi, A=""10",B="20")
+texto = Texto(nome_da_cena, txt = "Pergunta", foi = foii, A="resposta", B= "resposta")
 texto.vai()
 
-def foi(l):
-    r = Texto(nome_da_cena, txt=dicionario[l])
-    r.vai()
+
 
                               
