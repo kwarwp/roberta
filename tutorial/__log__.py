b2ebb18afb770cf4b64b44abed6f9e28fa6533b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 17:05:15 -0300

    Saved tutorial/__log__.py @2020-11-06 20:05

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -928,3 +928,19 @@ NameError: name 'STYLE' is not defined
     nome_do_elemento = Elemento(LIVRO, tit="título_do_elemento", 
 NameError: name 'funcao_de_acao_do_botao' is not defined
 '''},
+{'date': 'Fri Nov 06 2020 17:05:14.561 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    nome_do_elemento.elt.bind("click", self.funcao_de_acao_do_botao)
+AttributeError: 'GUI' object has no attribute 'funcao_de_acao_do_botao'
+'''},
