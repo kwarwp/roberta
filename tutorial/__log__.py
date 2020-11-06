24cbf34818262c4dc09685115b43cefc736d6766
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 18:17:34 -0300

    Saved tutorial/__log__.py @2020-11-06 21:17

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1109,3 +1109,19 @@ TypeError: 'bool' object is not iterable
     nome_da_cena.meio,esquerda,direita = texto_.vai()
 TypeError: 'bool' object is not iterable
 '''},
+{'date': 'Fri Nov 06 2020 18:17:32.996 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    texto_.entra(nome_da_cena)
+AttributeError: 'Texto' object has no attribute 'entra'
+'''},
