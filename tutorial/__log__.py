547b8eb4ab0f08885b1f23dc3d2fceac00899272
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 20:10:05 -0300

    Saved tutorial/__log__.py @2020-11-06 23:10

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1353,3 +1353,19 @@ IndentationError: unexpected indent
     STYLE["width"] = 900 # width = 300 (default) 
 NameError: name 'STYLE' is not defined
 '''},
+{'date': 'Fri Nov 06 2020 20:10:05.67 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    nome_da_musica = Musica(MINHA_MUSICA, loop = True, autoplay = True)
+NameError: name 'Musica' is not defined
+'''},
