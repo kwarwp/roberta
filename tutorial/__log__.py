2a0d40845fc3421e36c285c18ca813775a1f50c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-06 19:34:48 -0300

    Saved tutorial/__log__.py @2020-11-06 22:34

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1256,3 +1256,19 @@ SyntaxError: invalid syntax
                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 06 2020 19:34:47.860 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    texto = Texto(nome_da_cena, txt = "Pergunta", foi = foii, A="resposta", B= "resposta")
+NameError: name 'foii' is not defined
+'''},
