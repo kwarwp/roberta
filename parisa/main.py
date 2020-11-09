ca3e0efc857a4c4c7f8c993642dc1cfcfff38a7b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-09 20:56:32 -0300

    Saved parisa/main.py @2020-11-09 23:56

--- parisa/main.py
+++ parisa/main.py
@@ -1,15 +1,16 @@
 # roberta.parisa.main.py
-from _spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena, Inventario as inv 
 
 IMG_1 = "https://www.searchpng.com/wp-content/uploads/2019/01/Tiger-PNG-1024x1024.png"
 IMG_2 = "https://i.imgur.com/GleAY3f.jpeg"
 IMG_3 = 'https://i.imgur.com/4oUluVP.jpeg'
 
-cena_um = Cena(img = IMG_1, nome='Tigre', xy=(0, 0))
-
+cena_dois = Cena(img = IMG_2, xy=(0, 0))
+cena_tres = Cena(img = IMG_3, xy=(0, 0))
+cena_um = Cena(img = IMG_1, esquerda=cena_dois,direita=cena_tres,nome="Tigre",
+               score=dict(ponto=0, valor=0, carta=None, casa=None, move=None))
 
           #(self, img=IMAGEM, esquerda=NADA, direita=NADA, meio=NADA,
               #vai=None, nome='', xy=(0, 0), score=NOSC, **kwargs)
-                 
-
+inv.inicia()             
 cena_um.vai()
