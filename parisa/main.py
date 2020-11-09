3be88b3f53109e9302ffd8161793514e72355a2e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-09 20:43:03 -0300

    Created parisa/main.py @2020-11-09 23:43

---
+++ parisa/main.py
@@ -0,0 +1,15 @@
+# roberta.parisa.main.py
+from _spy.vitollino.main import Cena
+
+IMG_1 = "https://www.searchpng.com/wp-content/uploads/2019/01/Tiger-PNG-1024x1024.png"
+IMG_2 = "https://i.imgur.com/GleAY3f.jpeg"
+IMG_3 = 'https://i.imgur.com/4oUluVP.jpeg'
+
+cena_um = Cena(img = IMG_1, nome='Tigre', xy=(0, 0))
+
+
+          #(self, img=IMAGEM, esquerda=NADA, direita=NADA, meio=NADA,
+              #vai=None, nome='', xy=(0, 0), score=NOSC, **kwargs)
+                 
+
+cena_um.vai()
