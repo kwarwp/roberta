f4caf97e9f7a0973f99e300c71a583158c02a2e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-09 20:58:15 -0300

    Saved parisa/__score__.py @2020-11-09 23:58

--- parisa/__score__.py
+++ parisa/__score__.py
@@ -1,1 +1,2 @@
   {'doc_id': '60d4fcb4', 'carta': None, 'casa': None, 'move': (-100, -100), 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1604966248460},
+  {'doc_id': '60d4fcb4', 'carta': None, 'casa': None, 'move': (-100, -100), 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1604966293437},
