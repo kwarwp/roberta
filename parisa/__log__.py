bf746de6e558dce97b3b73a30adfca40dad8a9ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-09 20:56:02 -0300

    Saved parisa/__log__.py @2020-11-09 23:56

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -31,3 +31,19 @@ TypeError: __init__() got multiple values for argument 'img'
     invi.inicia()             
 NameError: name 'invi' is not defined
 '''},
+{'date': 'Mon Nov 09 2020 20:56:00.162 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    inv.inicia()             
+TypeError: inicia() missing 1 positional argument: self
+'''},
