d242bf3958e8b4b6039edbc3465be17a05b038e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-09 21:03:55 -0300

    Saved parisa/main.py @2020-11-10 00:03

--- parisa/main.py
+++ parisa/main.py
@@ -5,14 +5,17 @@ IMG_1 = "https://www.searchpng.com/wp-content/uploads/2019/01/Tiger-PNG-1024x102
 IMG_2 = "https://i.imgur.com/GleAY3f.jpeg"
 IMG_3 = 'https://i.imgur.com/4oUluVP.jpeg'
 
-cena_dois = Cena(img = IMG_2, xy=(0, 0))
+def pontua():
+    inv.score(casa=1, carta=2, move=3, ponto=4, valor=5, _level=1)
+    
+cena_pontos = Cena(vai=pontua)
+
+cena_dois = Cena(img = IMG_2, esquerda=cena_pontos, xy=(0, 0))
 cena_tres = Cena(img = IMG_3, xy=(0, 0))
+
 cena_um = Cena(img = IMG_1, esquerda=cena_dois,direita=cena_tres,nome="Tigre",
                score=dict(ponto=0, valor=0, carta=None, casa=None, move=None))
-
-          #(self, img=IMAGEM, esquerda=NADA, direita=NADA, meio=NADA,
-              #vai=None, nome='', xy=(0, 0), score=NOSC, **kwargs)
+       
+           #(self, casa, carta, move, ponto, valor, _level=1):
 inv.inicia()             
 cena_um.vai()
-
-print(cena_um.score())
