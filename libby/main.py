c9d83adbb5c450deabf7d9e6dbc9e8a00a224806
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-13 16:18:27 -0300

    Saved libby/main.py @2020-11-13 19:18

--- libby/main.py
+++ libby/main.py
@@ -9,37 +9,37 @@ Changelog
        - xxx
 
 """
-"""
+
+
 from _spy.vitollino.main import Cena
-variavel_global = 'https://i.imgur.com/P9mWuhm.jpeg'
+"""Importa a classe Cena do Vitollino"""
 
-def ambiente():
-    #variavel_local= 'https://i.imgur.com/lRpFqpe.jpeg'
-    floresta=Cena(variavel)
-    floresta.vai()
+IMAGEM_QUALQUER = 'https://i.imgur.com/Wn43yRS.jpeg' # Extensões aceitas: png, jpg, jpeg e gif
+IMAGEM_ESQUERDA = "https://i.imgur.com/8c5lNV5.jpg" # Extensões aceitas: png, jpg, jpeg e gif
+IMAGEM_DIREITA = "string_correspondente_a_url_e_extensao_da_image" # Extensões aceitas: png, jpg, jpeg e gif
+IMAGEM_MEIO = "string_correspondente_a_url_e_extensao_da_image" # Extensões aceitas: png, jpg, jpeg e gif
+
+nome_da_cena_direita = Cena(IMAGEM_DIREITA)
+nome_da_cena_esquerda = Cena(IMAGEM_ESQUERDA)
+nome_da_cena = Cena(IMAGEM_QUALQUER, # Parâmetro obrigatório
+                esquerda=nome_da_cena_esquerda, # default = NADA = SalaCenaNula()
+                direita=nome_da_cena_direita,  # default = NADA = SalaCenaNula()
+                meio=Cena(IMAGEM_MEIO)) # default = NADA = SalaCenaNula()
+nome_da_cena_esquerda.esquerda = nome_da_cena
+from _spy.vitollino.main import Cena, Elemento
+""" O botão é o elementoclicável de alguma cena.
 """
-"""
+MINHA_CENA = "string_correspondente_a_url_e_extensao_da_imagem" # Extensões aceitas: png, jpg, jpeg e gif
+MEU_ELEMENTO = "string_correspondente_a_url_e_extensao_da_imagem" # Extensões aceitas: png, jpg, jpeg e gif
 
-numero =20
+def funcao_de_acao_do_botao(event = None):
+    #Funcao chamada no clique
+    print("Você clicou no botão!") # evento associado ao clique: mensagem, cena, sala,módulo...
 
-def verifica():
-    if numero > 5:
-        print ('O numero é maior que cinco')
-    elif numero ==5:
-        print ('O numero é igual a cinco')
-    else:
-        print ('O numero é menor que cinco')
-        
-#verifica()
-
-"""
-"""
-
-animais = ['macaco', 'aguia', 'pardal', 'leao']
-
-for x in animais:
-    print (x)
-    if x== 'leao':
-        break
-    print (x)
-"""
+nome_da_cena = Cena(MINHA_CENA)
+nome_da_cena.vai() # instancia a cena
+nome_do_elemento = Elemento(MEU_ELEMENTO, tit="título_do_elemento",
+                           style=dict(height=60,widht=60, left=600, top=20), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = nome_da_cena,
+                           vai = funcao_de_acao_do_botao)
+nome_da_cena.vai()
