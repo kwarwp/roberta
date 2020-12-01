e767aa03211e83ed77238f4a75a45c3fbf58976e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 16:46:12 -0300

    Saved sarah/__log__.py @2020-12-01 19:46

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -349,3 +349,19 @@ NameError: name 'nome_do_elemento' is not defined
     nome_do_elemento.elt.bind("click", funcao_de_acao_do_botao)
 NameError: name 'nome_do_elemento' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 16:46:00.779 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    nome_do_elemento.elt.bind("click", funcao_de_acao_do_botao)
+NameError: name 'nome_do_elemento' is not defined
+'''},
