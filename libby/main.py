7b845c880318eac7f717735fbdd64c291c9b6c52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 17:01:17 -0300

    Saved libby/main.py @2020-12-01 20:01

--- libby/main.py
+++ libby/main.py
@@ -57,7 +57,4 @@ texto_.vai()
 LIVRO.elt.bind("click", funcao_de_acao_do_botao)
 
 
-LIVRO= Elemento(LIVRO, tit="título_do_elemento",
-                           style=dict(height=50,widht=56, left=450, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
-                           cena = cena1,
-                           vai = funcao_de_acao_do_botao)
+
