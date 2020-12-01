8caff1e78389282a7cd2ce8a336d18ef428bbabf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 16:44:18 -0300

    Saved libby/main.py @2020-12-01 19:44

--- libby/main.py
+++ libby/main.py
@@ -18,16 +18,25 @@ imagem_quarto = 'https://i.pinimg.com/originals/66/88/9a/66889a5a4db243c94e3c062
 cena1 = Cena(imagem_quarto)
 STYLE["width"] = 960 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
-LIVRO = 'https://livrariaconcreta.com.br/wp-content/uploads/2017/01/Hardcover-Book-MockUp-LIVRO-VERMELHO.png'
-def funcao_de_acao_do_botao(event = None):
-    texto_surpresa = Texto(cena1, txt ="Mensagem que você deseja passar!")
-    texto_surpresa.vai()
-
-
-cena1.vai() # instancia a cena
-botao_livro= Elemento(LIVRO, tit="título_do_elemento",
+imagem_livro = 'https://livrariaconcreta.com.br/wp-content/uploads/2017/01/Hardcover-Book-MockUp-LIVRO-VERMELHO.png'
+#def funcao_de_acao_do_botao(event = None):
+#    texto_surpresa = Texto(cena1, txt ="Mensagem que você deseja passar!")
+#    texto_surpresa.vai()
+def chama_elemento(*args):
+	LIVRO= Elemento(LIVRO, tit="título_do_elemento",
                            style=dict(height=50,widht=56, left=450, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = cena1,
                            vai = funcao_de_acao_do_botao)
-cena1.vai()
-nome_do_elemento.elt.bind("click", funcao_de_acao_do_botao)
+	LIVRO.entra(cena1)
+
+cena1.vai() # instancia a cena
+texto_ = Texto(cena1, txt = "Mensagem desejada", foi = funcao_do_elemento) # o método foi() esconde o popup
+texto_.vai()
+
+
+#LIVRO= Elemento(LIVRO, tit="título_do_elemento",
+#                           style=dict(height=50,widht=56, left=450, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+#                           cena = cena1,
+#                           vai = funcao_de_acao_do_botao)
+
+#nome_do_elemento.elt.bind("click", funcao_de_acao_do_botao)
