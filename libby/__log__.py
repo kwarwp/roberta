a19444ec1eb64fe30d2eb4bc1c00c5c10b439bb2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 16:44:47 -0300

    Saved libby/__log__.py @2020-12-01 19:44

--- libby/__log__.py
+++ libby/__log__.py
@@ -350,3 +350,19 @@ NameError: name 'funcao_do_elemento' is not defined
     texto_ = Texto(cena1, txt = "Mensagem desejada", foi = funcao_do_elemento) # o método foi() esconde o popup
 NameError: name 'funcao_do_elemento' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 16:44:44.978 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    texto_ = Texto(cena1, txt = "Mensagem desejada", foi = funcao_do_elemento) # o método foi() esconde o popup
+NameError: name 'funcao_do_elemento' is not defined
+'''},
