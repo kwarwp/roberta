770bad1383af027983e61119cafab60a2645d5a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 15:55:58 -0300

    Saved libby/main.py @2020-12-01 18:55

--- libby/main.py
+++ libby/main.py
@@ -16,7 +16,7 @@ from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
 imagem_quarto = 'https://i.pinimg.com/originals/66/88/9a/66889a5a4db243c94e3c0623df56e664.jpg'
 cena1 = Cena(imagem_quarto)
-STYLE["width"] = 960 #  width = 300 (default)
+STYLE["width"] = 1330  #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 LIVRO = 'https://livrariaconcreta.com.br/wp-content/uploads/2017/01/Hardcover-Book-MockUp-LIVRO-VERMELHO.png'
 def funcao_de_acao_do_botao(event = None):
