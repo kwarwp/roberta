8244b6fe3af239def5d31238d5efde6d2a247c8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 17:04:03 -0300

    Saved sarah/__log__.py @2020-12-01 20:04

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -724,3 +724,19 @@ NameError: name 'LIVRO' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Dec 01 2020 17:03:51.785 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    LIVRO= Elemento(LIVRO, tit="título_do_elemento",
+NameError: name 'LIVRO' is not defined
+'''},
