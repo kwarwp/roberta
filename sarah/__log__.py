1115f84b595be237c291fdfbef85e3fbddd4259e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 17:07:07 -0300

    Saved sarah/__log__.py @2020-12-01 20:07

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -756,3 +756,19 @@ NameError: name 'LIVRO' is not defined
     LIVRO = Elemento(LIVRO, tit="título_do_elemento",
 NameError: name 'LIVRO' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 17:06:54.652 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    LIVRO = Elemento(imagem_livro, tit="título_do_elemento",
+NameError: name 'funcao_de_acao_do_botao' is not defined
+'''},
