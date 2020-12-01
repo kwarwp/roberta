227de47182bedfbe83d64596f717f09c83b7f90f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 17:33:24 -0300

    Saved stacy/__log__.py @2020-12-01 20:33

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -15,3 +15,19 @@
     LIVRO= Elemento(imagem_livro, tit="título_do_elemento",
 NameError: name 'funcao_de_acao_do_botao' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 17:33:22.796 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    LIVRO= Elemento(imagem_livro, tit="título_do_elemento",
+NameError: name 'funcao_de_acao_do_botao' is not defined
+'''},
