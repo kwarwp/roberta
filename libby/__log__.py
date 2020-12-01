14301d095d5b595fb87861ede32b30c2700b523e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 15:53:54 -0300

    Saved libby/__log__.py @2020-12-01 18:53

--- libby/__log__.py
+++ libby/__log__.py
@@ -203,3 +203,10 @@ SyntaxError: invalid syntax
                                                                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Dec 01 2020 15:53:52.219 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 28
+  botao_livro= Elemento(LIVRO, tit="título_do_elemento",
+                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
