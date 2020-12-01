ab8d644f1c5229048b9d214ca5af6bb8728bc269
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 17:47:49 -0300

    Saved libby/__log__.py @2020-12-01 20:47

--- libby/__log__.py
+++ libby/__log__.py
@@ -572,3 +572,10 @@ NameError: name 'funcao_de_acao_do_botao' is not defined
     LIVRO= Elemento(imagem_livro, tit="título_do_elemento",
 NameError: name 'funcao_de_acao_do_botao' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 17:47:47.396 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  def mostra_cena(self, *_ ):
+  ^
+IndentationError: unexpected indent
+'''},
