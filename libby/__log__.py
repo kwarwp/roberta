28e670bde8a9c62f0df6c2fdab1cab3a1a72370e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 12:45:34 -0300

    Saved libby/__log__.py @2020-12-01 15:45

--- libby/__log__.py
+++ libby/__log__.py
@@ -89,3 +89,19 @@ NotImplementedError: Blocking functions like time.sleep() are not supported in t
     ATO1CENA1 = Cena(IMAGEM_QUARTO, 
 NameError: name 'SalaCenaNula' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 12:45:33.275 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    ATO1CENA1 = Cena(IMAGEM_QUARTO, 
+NameError: name 'SalaCenaNula' is not defined
+'''},
