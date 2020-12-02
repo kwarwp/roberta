ec87d4b72718d6dfae0c674929d8749e1e6b1d06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-02 14:18:13 -0300

    Saved libby/main.py @2020-12-02 17:18

--- libby/main.py
+++ libby/main.py
@@ -65,7 +65,7 @@ LIVRO= Elemento(imagem_livro, tit="título_do_elemento",
                 
                 
 PAPEL_RASGADO = Elemento(papel_rasgado, tit="título_do_elemento",
-                           style=dict(height='500px' ,widht='30000px', left=450, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           style=dict(height='500px' ,widht=30000, left=450, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = cena2)
 
 #ELEMENTOS
