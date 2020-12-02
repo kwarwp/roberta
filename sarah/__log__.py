932a220a38dee8bae68e601bae4bdee093923e66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-02 18:05:26 -0300

    Saved sarah/__log__.py @2020-12-02 21:05

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -932,3 +932,19 @@ NameError: name 'nome_da_cena' is not defined
     SETA = Elemento(imagem_seta, tit="título_do_elemento",
 NameError: name 'nome_da_cena' is not defined
 '''},
+{'date': 'Wed Dec 02 2020 18:05:11.439 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+ImportError: _spy
+'''},
