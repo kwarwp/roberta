1bf08ba283b36c9a245c5dab8cef27083a5900d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-01 23:24:27 -0300

    Saved libby/__log__.py @2020-12-02 02:24

--- libby/__log__.py
+++ libby/__log__.py
@@ -674,3 +674,19 @@ NameError: name 'LIVRO' is not defined
     LIVRO.elt.bind("click", funcao_de_acao_do_botao)
 NameError: name 'LIVRO' is not defined
 '''},
+{'date': 'Tue Dec 01 2020 23:24:24.925 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    LIVRO.elt.bind("click", funcao_de_acao_do_botao)
+NameError: name 'LIVRO' is not defined
+'''},
