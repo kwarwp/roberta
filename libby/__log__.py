ff1dfab8f06d8e939b7c6b15f649f32c03a03b35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-03 13:59:30 -0300

    Saved libby/__log__.py @2020-12-03 16:59

--- libby/__log__.py
+++ libby/__log__.py
@@ -855,3 +855,19 @@ SyntaxError: invalid syntax
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Dec 03 2020 13:59:27.151 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 85
+    pergunta = Texto(cena3, txt = "Qual e a resposta paraa o desafio?", foi = resultado, A=input(txt))
+NameError: name 'txt' is not defined
+'''},
