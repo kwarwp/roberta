562e0ec1852b6c231dc6fcecba388a06e776b9bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-03 13:35:23 -0300

    Saved libby/__log__.py @2020-12-03 16:35

--- libby/__log__.py
+++ libby/__log__.py
@@ -729,3 +729,19 @@ SyntaxError: keyword can't be an expression
     resposta_desafio1.vai()
 AttributeError: 'str' object has no attribute 'vai'
 '''},
+{'date': 'Thu Dec 03 2020 13:35:19.978 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 94
+    resposta_desafio1.vai()
+AttributeError: 'str' object has no attribute 'vai'
+'''},
