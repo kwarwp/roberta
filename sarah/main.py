b5cc6b6f852512aacd56e4996d7e94f8b309bee3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-02 21:15:09 -0300

    Saved sarah/main.py @2020-12-03 00:15

--- sarah/main.py
+++ sarah/main.py
@@ -19,9 +19,9 @@ imagem_computador2 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOkAAADZCAMA
 STYLE["width"] = 450 # width = 300 (default)
 STYLE["heigth"] = "450px" # min-height = "300px"
 
-def clique_computador(event = None):
+def clique_computador(event = imagem_computador2):
     #Funcao chamada no clique
-    print("Voce clicou no botao!") # evento associado ao clique: mensagem, cena, sala,módulo...
+    event("Cena2") # evento associado ao clique: mensagem, cena, sala,módulo...
 
 Cena1 = Cena(imagem_computador)
 Cena1.vai() # instancia a cena
