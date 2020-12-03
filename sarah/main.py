d58a4a234f0a56048a83aa5b9f8a3bebdbb824fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-02 21:19:28 -0300

    Saved sarah/main.py @2020-12-03 00:19

--- sarah/main.py
+++ sarah/main.py
@@ -19,13 +19,14 @@ imagem_computador2 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOkAAADZCAMA
 STYLE["width"] = 450 # width = 300 (default)
 STYLE["heigth"] = "450px" # min-height = "300px"
 
+Cena2 = Cena(imagem_computador2)
+Cena2.vai()
+
 def clique_computador(event = Cena2):
-    Cena2 = Cena(imagem_computador2)
-    Cena2.vai()
     print("Next step") # evento associado ao clique: mensagem, cena, sala,módulo...
 
 Cena1 = Cena(imagem_computador)
-Cena1.vai() # instancia a cena
+Cena1.vai()
 Seta1 = Elemento(botao_seta, tit="CLIQUE",
                            style=dict(height=100,widht=100, left=300, top=250), # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = Cena1,
