f6d282a4f6334413cdd51b648f6258088856f336
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 16:10:42 -0300

    Saved libby/main.py @2020-12-04 19:10

--- libby/main.py
+++ libby/main.py
@@ -79,7 +79,6 @@ PAPEL_RASGADO = Elemento(papel_rasgado, tit="título_do_elemento",
 #resposta_desafio1= input('Qual é a resposta do desafio?')
 
 def desafio1(resposta2):
-    possivel_resposta=""
     resposta1=input('Qual é a resposta do desafio?')
     resposta2=resposta1.lower()
     if resposta2 == 'va para a biblioteca' or 'vá para a biblioteca' :
