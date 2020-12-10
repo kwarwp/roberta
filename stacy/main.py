28beb80ddf387bf384f1e12b107a738cc51ed110
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-10 17:28:02 -0300

    Saved stacy/main.py @2020-12-10 20:28

--- stacy/main.py
+++ stacy/main.py
@@ -1,6 +1,13 @@
 
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 
+Imagem_Biblioteca_porta="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgartic.com.br%2Fflavicastelo%2Fdesenho-jogo%2Fporta&psig=AOvVaw3-LiWhR8txfHBaY4ILI-gD&ust=1607717882764000&source=images&cd=vfe&ved=2ahUKEwia-9b7ncTtAhX0L7kGHXqKBUsQjRx6BAgAEAc"
+Imagem_Biblioteca_porta2="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Fvetores-premium%2Fporta-aberta-dos-desenhos-animados-entrada-do-corredor-do-apartamento-entrada-do-escritorio_6332551.htm&psig=AOvVaw3FO5aZ1pS7gFXDd_4hhYsM&ust=1607718022573000&source=images&cd=vfe&ved=2ahUKEwj2may-nsTtAhXQNLkGHUZAAc0QjRx6BAgAEAc"
+Imagem_Biblioteca_dentro="https://www.google.com/url?sa=i&url=https%3A%2F%2Fsecult.es.gov.br%2Fbiblioteca-publica-do-espirito-santo-bpes&psig=AOvVaw00OvZAC3CNZjJY6igg8dXy&ust=1607718073857000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjkudmexO0CFQAAAAAdAAAAABAD"
 
+STYLE["width"] = 960 #  width = 300 (default)
+STYLE["heigth"] = '600px' # min-height = "300px"
 
-
+Biblioteca_entrada=Cena(Imagem_Biblioteca_porta)
+Biblioteca_entrada.vai()
 
