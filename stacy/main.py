1383f41bdb5f92f20c258a1a86558361171ea988
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-10 17:33:21 -0300

    Saved stacy/main.py @2020-12-10 20:33

--- stacy/main.py
+++ stacy/main.py
@@ -5,7 +5,7 @@ Imagem_Biblioteca_porta="https://media.istockphoto.com/vectors/closed-door-drawi
 Imagem_Biblioteca_porta2="https://image.freepik.com/vetores-gratis/porta-aberta-dos-desenhos-animados-entrada-do-corredor-do-apartamento-entrada-do-escritorio_53562-8532.jpg"
 Imagem_Biblioteca_dentro="https://secult.es.gov.br/Media/secult/BPES/IMG_7650.JPG"
 
-STYLE["width"] = 960 #  width = 300 (default)
+STYLE["width"] = 500 #  width = 300 (default)
 STYLE["heigth"] = '600px' # min-height = "300px"
 
 Biblioteca_entrada=Cena(Imagem_Biblioteca_porta)
