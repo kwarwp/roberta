c89f565b457662d4e5e5e77721191362352ac273
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-10 18:01:03 -0300

    Saved stacy/__log__.py @2020-12-10 21:01

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -47,3 +47,19 @@ NameError: name 'funcao_de_acao_do_botao' is not defined
     BOTAO= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
 NameError: name 'Biblioteca_entra' is not defined
 '''},
+{'date': 'Thu Dec 10 2020 18:01:01.954 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    BOTAO= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+NameError: name 'Biblioteca_entra' is not defined
+'''},
