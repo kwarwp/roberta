1496d92160bc7611f271f89c5813a2e62d200dbd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-11 16:07:24 -0300

    Saved stacy/__log__.py @2020-12-11 19:07

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -111,3 +111,19 @@ NameError: name 'Biblioteca_entra' is not defined
     BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
 NameError: name 'Biblioteca_porta2' is not defined
 '''},
+{'date': 'Fri Dec 11 2020 16:07:23.167 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    BOTAO2= Elemento(Imagem_botao, tit="click",w=30,h=36,  x=450, y=300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+NameError: name 'Biblioteca_porta1' is not defined
+'''},
