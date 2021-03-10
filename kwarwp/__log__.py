ef2e410f4bad105d0028854dd41c09153f3fe69f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-09 21:25:56 -0300

    Saved kwarwp/__log__.py @2021-03-10 00:25

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -205,3 +205,21 @@ SyntaxError: invalid syntax
     return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=API, indios=(api,))
 NameError: name 'api' is not defined
 '''},
+{'date': 'Tue Mar 09 2021 21:25:55.200 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    main(Jogo, STYLE)
+  module <module> line 106
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=APA, indios=(Ap,))
+NameError: name 'Ap' is not defined
+'''},
