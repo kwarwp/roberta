817e6a919ac64816b118e33c5389a167ee58033a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-07 11:47:07 -0300

    Saved tutorial/__log__.py @2021-04-07 14:47

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1385,3 +1385,19 @@ NameError: name 'Musica' is not defined
     tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore",
 NameError: name 'nome_da_cena' is not defined
 '''},
+{'date': 'Wed Apr 07 2021 11:47:06.90 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore",
+NameError: name 'nome_da_cena' is not defined
+'''},
