6a1156e95a38688d4a596218926e6ca95adfff03
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-08 22:07:11 -0300

    Saved tutorial/teste_game.py @2021-04-09 01:07

--- tutorial/teste_game.py
+++ tutorial/teste_game.py
@@ -1,14 +1,11 @@
 # roberta.tutorial.teste_game.py
 # SPDX-License-Identifier: GPL-3.0-or-later
 """ Projeto de TCC: COORDENAÇÕES LÓGICAS
-
 .. codeauthor:: Emanuelle Simas <ellesimas@gmail.com>
-
 Changelog
 ---------
 .. versionadded::    21.04
        Teste de tamanho das peças
-
 """
 BACKGROUND = "https://i.imgur.com/15vsHKm.jpg"
 
@@ -54,20 +51,35 @@ ramo_criterio_um = Elemento(BRANCH_UM_CRITERIADO,  x =379, y=193, w=175, h=175,c
 
 """
 criteriado = Elemento(GEOMETRICO_CIRCULO_AZUL, x=370, y=245, cena = fundo_estatico)
-
 criterio_azul = Elemento(CRITERIO_AZUL, x=450, y=350,  cena = fundo_estatico)
 criterio_circulo = Elemento(CRITERIO_CIRCULO, x=450, y=350,  cena = fundo_estatico)
 criterio_grande = Elemento(CRITERIO_GRANDE, x=450, y=350,  cena = fundo_estatico)
-
        
 ramo_criteriado_tres_nivel_um = Elemento(BRANCH_TRES_CRITERIO_NIVEL_UM,  x =379, y=193,cena = fundo_estatico)
-
 ramo_criterio_tres_nivel_dois = Elemento(BRANCH_TRES_CRITERIADO_NIVEL_DOIS,  x =379, y=193,cena = fundo_estatico)
-
 ramo_criterio_um = Elemento(BRANCH_UM_CRITERIADO,  x =379, y=193,cena = fundo_estatico)
-
-
 tronco_base = Elemento(TRONCO_BASE, x =300, y= 365,  cena = fundo_estatico)        
 """
+                           
+fundo_estatico.vai()
+
+
+fundo_estatico = Cena(BACKGROUND)
+criteriado = Elemento(GEOMETRICO_CIRCULO, tit="Este é o tronco da árvore",x=370, y=245, w = 30, h=30, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+
+criterio_vermelho = Elemento(CRITERIO_VERMELHO, tit="Este é o tronco da árvore", x=450, y=350, w=20, h=20, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+                           
+#ramo_criterio_tres = Elemento(BRANCH_TRES_CRITERIO, tit="Este é o tronco da árvore", x =375, y=197, w=170, h=170, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+#                           cena = fundo_estatico)
+                           
+ramo_criteriado_tres = Elemento(BRANCH_TRES_CRITERIADO, tit="Este é o tronco da árvore", x =379, y=193, w=175, h=175, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+
+tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore", x =300, y= 365, w = 300, h = 300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+                           
+                   
                            
 fundo_estatico.vai()
