bba26ebf8821f2f81015dac1eb3f4077643fa664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-08 21:14:25 -0300

    Saved tutorial/teste_game.py @2021-04-09 00:14

--- tutorial/teste_game.py
+++ tutorial/teste_game.py
@@ -10,12 +10,23 @@ Changelog
        Teste de tamanho das peças
 
 """
-BACKGROUND = "https://i.imgur.com/15vsHKm.jpg"
+"""
+
 TRONCO_BASE = "https://i.imgur.com/FiV74AD.png"
 BRANCH_TRES_CRITERIO = "https://i.imgur.com/E4VF4sb.png"
 BRANCH_TRES_CRITERIADO = "https://i.imgur.com/AC8DivF.png"
 CRITERIO_VERMELHO = "https://i.imgur.com/KU344jS.png"
 GEOMETRICO_CIRCULO = "https://i.imgur.com/Ch4uiED.png"
+"""
+BACKGROUND = "https://i.imgur.com/15vsHKm.jpg"
+TRONCO_BASE = "https://imgur.com/MvuKMHs.png"
+BRANCH_TRES_CRITERIO = "https://imgur.com/RNrW0L9.png"
+BRANCH_TRES_CRITERIADO = "https://imgur.com/Sl5USH5.png"
+BRANCH_UM_CRITERIO = "https://imgur.com/9HEwnOX.png"
+CRITERIO_AZUL = "https://imgur.com/bSutiDv.png"
+CRITERIO_CIRCULO = "https://imgur.com/lWXEv57.png"
+CRITERIO_GRANDE = "https://imgur.com/yBD04Wg.png"
+GEOMETRICO_CIRCULO_AZUL = "https://i.imgur.com/Ch4uiED.png"
 
 from browser import document
 pdiv = document["pydiv"]
@@ -31,25 +42,33 @@ STYLE.update(width = 1920, height = '1080px') # width = 300 (default)
 
 
 fundo_estatico = Cena(BACKGROUND)
-criteriado = Elemento(GEOMETRICO_CIRCULO, tit="Este é o tronco da árvore",x=370, y=245, w = 30, h=30, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+
+criteriado = Elemento(GEOMETRICO_CIRCULO_AZUL, tit="Este é o tronco da árvore",x=370, y=245, w = 30, h=30, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)
 
-criterio_vermelho = Elemento(CRITERIO_VERMELHO, tit="Este é o tronco da árvore", x=450, y=350, w=20, h=20, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+criterio_vermelho = Elemento(CRITERIO_AZUL, tit="Este é o tronco da árvore", x=450, y=350, w=20, h=20, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+criterio_circulo= Elemento(CRITERIO_CIRCULO, tit="Este é o tronco da árvore", x=450, y=350, w=20, h=20, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+criterio_grande = Elemento(CRITERIO_GRANDE, tit="Este é o tronco da árvore", x=450, y=350, w=20, h=20, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)
                            
 #ramo_criterio_tres = Elemento(BRANCH_TRES_CRITERIO, tit="Este é o tronco da árvore", x =375, y=197, w=170, h=170, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
 #                           cena = fundo_estatico)
                            
+ramo_criterio_tres = Elemento(BRANCH_TRES_CRITERIO, tit="Este é o tronco da árvore", x =379, y=193, w=175, h=175, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+                           cena = fundo_estatico)
+                           
 ramo_criteriado_tres = Elemento(BRANCH_TRES_CRITERIADO, tit="Este é o tronco da árvore", x =379, y=193, w=175, h=175, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)
 
-tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore", x =300, y= 365, w = 300, h = 300, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore", x =300, y= 700, w = 400, h = 400, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)
                            
                            
                            
                            
-                           
+"""                           
 criteriado = Elemento(GEOMETRICO_CIRCULO, tit="Este é o tronco da árvore",x=100, y=245, w=100, h=100,  # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)
 
@@ -64,6 +83,6 @@ ramo_criteriado_tres = Elemento(BRANCH_TRES_CRITERIADO, tit="Este é o tronco da
 
 tronco_base = Elemento(TRONCO_BASE, tit="Este é o tronco da árvore", x =100, y= 365, w=100, h=100, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
                            cena = fundo_estatico)                      
-                          
+"""                          
                            
 fundo_estatico.vai()
