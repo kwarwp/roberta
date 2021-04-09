edc5f07ee685b3af20246c3f3fb9468d7fd8cab3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-08 21:51:24 -0300

    Saved tutorial/__log__.py @2021-04-09 00:51

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1484,3 +1484,19 @@ SyntaxError: keyword argument repeated
     pdiv- document["pydiv"]
 NameError: name 'pdiv' is not defined
 '''},
+{'date': 'Thu Apr 08 2021 21:51:24.343 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    ramo_criteriado_tres = Elemento(BRANCH_TRES_CRITERIADO,  x =379, y=193, w=175, h=175,cena = fundo_estatico)
+NameError: name 'BRANCH_TRES_CRITERIADO' is not defined
+'''},
