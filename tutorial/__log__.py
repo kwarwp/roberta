aa227e43060c11281c630bc20014a6d6835d78b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-08 22:07:07 -0300

    Saved tutorial/__log__.py @2021-04-09 01:07

--- tutorial/__log__.py
+++ tutorial/__log__.py
@@ -1516,3 +1516,19 @@ NameError: name 'BRANCH_TRES_CRITERIADO' is not defined
     criteriado = Elemento(GEOMETRICO_CIRCULO, tit="Este é o tronco da árvore",x=370, y=245, w = 30, h=30, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
 NameError: name 'GEOMETRICO_CIRCULO' is not defined
 '''},
+{'date': 'Thu Apr 08 2021 22:07:06.564 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    criteriado = Elemento(GEOMETRICO_CIRCULO, tit="Este é o tronco da árvore",x=370, y=245, w = 30, h=30, # ou x=eixo_x, y=eixo_y, w=largura, h=altura
+NameError: name 'GEOMETRICO_CIRCULO' is not defined
+'''},
