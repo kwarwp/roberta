b65cb916ae21443d5ba3bf1599dee3c8ea3fd64b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-08 21:54:06 -0300

    Saved tutorial/teste_game.py @2021-04-09 00:54

--- tutorial/teste_game.py
+++ tutorial/teste_game.py
@@ -34,10 +34,8 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 STYLE.update(width = 1920, height = '1080px') # width = 300 (default)
 #STYLE["heigth"] = "300px" # min-height = "300px"
 
-
-
 fundo_estatico = Cena(BACKGROUND)
-
+""""
 criteriado = Elemento(GEOMETRICO_CIRCULO_AZUL, x=370, y=245, w = 30, h=30, cena = fundo_estatico)
 
 criterio_azul = Elemento(CRITERIO_AZUL, x=450, y=350, w=20, h=20,  cena = fundo_estatico)
@@ -53,7 +51,21 @@ ramo_criterio_um = Elemento(BRANCH_UM_CRITERIADO,  x =379, y=193, w=175, h=175,c
 
 
 tronco_base = Elemento(TRONCO_BASE, x =300, y= 365, w = 300, h = 300,  cena = fundo_estatico)
-                           
-                   
+"""                          
+criteriado = Elemento(GEOMETRICO_CIRCULO_AZUL, x=370, y=245, cena = fundo_estatico)
+
+criterio_azul = Elemento(CRITERIO_AZUL, x=450, y=350,  cena = fundo_estatico)
+criterio_circulo = Elemento(CRITERIO_CIRCULO, x=450, y=350,  cena = fundo_estatico)
+criterio_grande = Elemento(CRITERIO_GRANDE, x=450, y=350,  cena = fundo_estatico)
+
+       
+ramo_criteriado_tres_nivel_um = Elemento(BRANCH_TRES_CRITERIO_NIVEL_UM,  x =379, y=193,cena = fundo_estatico)
+
+ramo_criterio_tres_nivel_dois = Elemento(BRANCH_TRES_CRITERIADO_NIVEL_DOIS,  x =379, y=193,cena = fundo_estatico)
+
+ramo_criterio_um = Elemento(BRANCH_UM_CRITERIADO,  x =379, y=193,cena = fundo_estatico)
+
+
+tronco_base = Elemento(TRONCO_BASE, x =300, y= 365,  cena = fundo_estatico)                   
                            
 fundo_estatico.vai()
