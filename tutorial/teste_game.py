98e89cd117fa5ea03f37384403bff1cf3fe141f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-08 00:21:10 -0300

    Saved tutorial/teste_game.py @2021-05-08 03:21

--- tutorial/teste_game.py
+++ tutorial/teste_game.py
@@ -35,15 +35,15 @@ STYLE.update(width = 1920, height = '1080px') # width = 300 (default)
 
 fundo_estatico        = Cena    (BACKGROUND)
 
-tronco_base           = Elemento(TRONCO_BASE                , x =550, y=550, w=416, h=523, cena = fundo_estatico)#
-ramo_nivel_tres       = Elemento(BRANCH_TRES_NIVEL_UM       , x =402, y=593, w=169, h=239, cena = fundo_estatico)#
-ramo_nivel_tres__     = Elemento(BRANCH_TRES_NIVEL_DOIS     , x =433, y=830, w=160, h=98 , cena = fundo_estatico)#
-ramo_nivel_tres_um    = Elemento(BRACO_UM_UM                , x =561, y=892, w=81 , h=100, cena = fundo_estatico)
-ramo_nivel_tres_dois  = Elemento(BRACO_UM_DOIS              , x =640, y=410, w=52 , h=93 , cena = fundo_estatico)
+tronco_base           = Elemento(TRONCO_BASE                , x =550, y=550, w=416, h=523, cena = fundo_estatico)# ok
+ramo_nivel_tres       = Elemento(BRANCH_TRES_NIVEL_UM       , x =402, y=593, w=169, h=239, cena = fundo_estatico)# ok
+ramo_nivel_tres__     = Elemento(BRANCH_TRES_NIVEL_DOIS     , x =433, y=830, w=160, h=98 , cena = fundo_estatico)# ok
+ramo_nivel_tres_um    = Elemento(BRACO_UM_UM                , x =561, y=892, w=81 , h=100, cena = fundo_estatico)# ok
+ramo_nivel_tres_dois  = Elemento(BRACO_UM_DOIS              , x =640, y=800, w=52 , h=93 , cena = fundo_estatico)
 ramo_nivel_tres_dois  = Elemento(BRACO_UM_TRES              , x =640, y=410, w=98 , h=85 , cena = fundo_estatico)
 
 criterio_cor_vermelho = Elemento(CRITERIO_COR_VERMELHO      , x =640, y=410, w=52 , h=52 , cena = fundo_estatico)
-criterio_circulo_A    = Elemento(CRITERIO_CIRCULO_A         , x =535, y=672, w=52 , h=52 , cena = fundo_estatico)#
+criterio_circulo_A    = Elemento(CRITERIO_CIRCULO_A         , x =535, y=672, w=52 , h=52 , cena = fundo_estatico)# ok
 geometrico_vermelho   = Elemento(GEOMETRICO_CIRCULO_VERMELHO, x =640, y=410, w=33 , h=33 , cena = fundo_estatico)
 
 
